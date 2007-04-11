254b2357f95ea9b956d27cad65df1a4e4b2d0ea2
Erlend Simonsen <admin@clockingit.com>
2007-04-11 08:51:24 +0000

    [ClockingIT @ Add [[wikiword|wiki word]] syntax for links]

--- app/models/wiki_page.rb
+++ app/models/wiki_page.rb
@@ -4,6 +4,11 @@ class WikiPage < ActiveRecord::Base
   belongs_to :company
 
   LOCKING_PERIOD = 30.minutes
+  CamelCase = Regexp.new( '\b((?:[A-Z]\w+){2,})' )
+  WIKI_LINK = /\[\[\s*([^\]\s][^\]]+?)\s*\]\]/
+#  WIKI_LINK = Regexp.new('\[\[([^\]]+?)\]\]')
+#  LINK_TYPE_SEPARATION = Regexp.new('^(.+):((file)|(pic))$', 0, 'utf-8')
+  ALIAS_SEPARATION = Regexp.new('^(.+)\|(.+)$', 0, 'utf-8')
 
   def lock(time, locked_by)
     update_attributes(:locked_at => time, :locked_by => locked_by)
@@ -27,6 +32,21 @@ class WikiPage < ActiveRecord::Base
 
   def to_html
     body = current_revision.body
+
+
+    body.gsub!( WIKI_LINK ) { |m|
+      match = m.match(WIKI_LINK)
+      name = text = match[1]
+
+      alias_match = match[1].match(ALIAS_SEPARATION)
+      if alias_match
+        name = alias_match[1]
+        text = alias_match[2]
+      end
+
+      "\"#{text}\":/wiki/show/#{URI.encode(name)}"
+    }
+#    body.gsub!( CamelCase, '"\1":/wiki/show/\1')
 
     RedCloth.new(body).to_html
   end
