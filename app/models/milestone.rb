b5bb0f5558da3961407a44de6304518acde7eb48
Erlend Simonsen <admin@clockingit.com>
2007-04-19 18:33:57 +0000

    [ClockingIT @ Don't show owner unless milestone has one]

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -29,7 +29,7 @@ class Milestone < ActiveRecord::Base
     res = ""
     res << "<strong>#{_('Name')}</strong> #{self.name}<br />"
     res << "<strong>#{_('Due Date')}</strong> #{self.due_at.strftime("%A, %d %B %Y")}<br/>" unless self.due_at.nil?
-    res << "<strong>#{_('Owner')}</strong> #{self.user.name}<br />"
+    res << "<strong>#{_('Owner')}</strong> #{self.user.name}<br />" unless self.user.nil?
     res << "<strong>#{_('Progress')}</strong> #{self.completed_tasks.to_i} / #{self.total_tasks.to_i} #{_('Complete')}"
     res << "<div class=tip_description> #{self.description.gsub(/\n/, '<br/>').gsub(/\"/,'&quot;')}</div>" if( self.description && self.description.strip.length > 0)
     res
