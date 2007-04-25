70c9a0609e9829a71b9f22b047fb2b3a43750c50
Tomás Lucas <tomaslucas@gmail.com>
2007-04-25 23:13:32 +0000

    [ClockingIT @ CamelCase]

--- app/models/wiki_revision.rb
+++ app/models/wiki_revision.rb
@@ -2,6 +2,7 @@ class WikiRevision < ActiveRecord::Base
   belongs_to :wiki_page
   belongs_to :user
 
+  EXPR = Regexp.new( '\b((?:[A-Z]\w+){2,})|\[\[\s*([^\]\s][^\]]+?)\s*\]\]' )
   CamelCase = /\b((?:[A-Z]\w+){2,})/
   WIKI_LINK = /\[\[\s*([^\]\s][^\]]+?)\s*\]\]/
 #  LINK_TYPE_SEPARATION = Regexp.new('^(.+):((file)|(pic))$', 0, 'utf-8')
@@ -51,38 +52,38 @@ class WikiRevision < ActiveRecord::Base
 
   def to_html
 
-    body.gsub!( CamelCase ) { |m|
-      url = "/wiki/show/#{URI.encode(m)}"
-      url_class = 'internal'
-      url_class << '_missing' unless WikiPage.find(:first, :conditions => ['company_id = ? and name = ?', self.wiki_page.company_id, m])
-
-
-      "%(#{url_class})\"#{m}\":#{url}%"
-   }
-
-    body.gsub!( WIKI_LINK ) { |m|
+    body.gsub!( EXPR ) { |m|
       match = m.match(WIKI_LINK)
-      name = text = match[1]
+      if match
+        name = text = match[1]
 
-      alias_match = match[1].match(ALIAS_SEPARATION)
-      if alias_match
-        name = alias_match[1]
-        text = alias_match[2]
-      end
+        alias_match = match[1].match(ALIAS_SEPARATION)
+        if alias_match
+          name = alias_match[1]
+          text = alias_match[2]
+        end
 
-      name.strip!
+        name.strip!
 
-      if name.downcase.include? '://'
-        url = name
-        url_class = 'external'
-      else
-        url = "/wiki/show/#{URI.encode(name)}"
+        if name.downcase.include? '://'
+          url = name
+          url_class = 'external'
+        else
+          url = "/wiki/show/#{URI.encode(name)}"
+          url_class = 'internal'
+          url_class << '_missing' unless WikiPage.find(:first, :conditions => ['company_id = ? and name = ?', self.wiki_page.company_id, name])
+        end
+
+        "%(#{url_class})\"#{text}\":#{url}%"
+   
+      else     
+        url = "/wiki/show/#{URI.encode(m)}"
         url_class = 'internal'
-        url_class << '_missing' unless WikiPage.find(:first, :conditions => ['company_id = ? and name = ?', self.wiki_page.company_id, name])
-      end
+        url_class << '_missing' unless WikiPage.find(:first, :conditions => ['company_id = ? and name = ?', self.wiki_page.company_id, m])
 
-      "%(#{url_class})\"#{text}\":#{url}%"
-    }
+        "%(#{url_class})\"#{m}\":#{url}%"
+      end
+   }
 
 
     body.gsub!( TaskNumber, '"#\1":/tasks/view/\1')
