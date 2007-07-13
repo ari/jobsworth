6af96f85dfdb069c5e586d71b079e8b3bcd95fb4
Erlend Simonsen <admin@clockingit.com>
2007-07-13 11:12:12 +0000

    [ClockingIT @ Show repeat parameters along with due date]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -134,6 +134,10 @@ module ApplicationHelper
       end
     end
 
+    if task.repeat && task.repeat.length > 0
+      res += ", " + task.repeat_summary
+    end
+
     if res.length > 0
       res = "<span class=\"" + css + "\">[" + res + "]</span>"
     end
