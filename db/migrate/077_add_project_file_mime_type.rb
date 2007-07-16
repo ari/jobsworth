0e48d3a5753a9ede4cd6e6028b3df4d90fe2aae5
Erlend Simonsen <admin@clockingit.com>
2007-07-16 16:13:52 +0000

    [ClockingIT @ Add mime-type to project_files]

---
+++ db/migrate/077_add_project_file_mime_type.rb
@@ -0,0 +1,26 @@
+class AddProjectFileMimeType < ActiveRecord::Migration
+
+  require 'RMagick'
+
+
+  def self.up
+    add_column :project_files, :mime_type, :string, :default => 'application/octet-stream'
+
+    execute("update project_files set mime_type='application/octet-stream'")
+
+    ProjectFile.find(:all).each do |f|
+      if f.thumbnail?
+        image = Magick::Image.read(f.file_path).first
+        puts "Setting mime_type=#{image.mime_type}"
+        f.mime_type = image.mime_type
+        f.save
+      end
+    end
+
+
+  end
+
+  def self.down
+    remove_column :project_files, :mime_type
+  end
+end
