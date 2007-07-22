2ad0f74f9b8d2c542b3c2dea47ec469acfaa4276
Erlend Simonsen <admin@clockingit.com>
2007-07-22 09:10:14 +0000

    [ClockingIT @ Add missing ; so asset_packager can compress prototype.js]

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -843,7 +843,7 @@ Ajax.Request.prototype = Object.extend(new Ajax.Base(), {
     }
 
     params = Hash.toQueryString(params);
-    if (params && /Konqueror|Safari|KHTML/.test(navigator.userAgent)) params += '&_='
+    if (params && /Konqueror|Safari|KHTML/.test(navigator.userAgent)) params += '&_=';
 
     // when GET, append parameters to URL
     if (this.method == 'get' && params)
