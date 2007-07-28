63966d33f3dee029577f7957fac9dc9146e9ff8d
Erlend Simonsen <admin@clockingit.com>
2007-07-28 19:21:33 +0000

    [ClockingIT @ Fix topics avatars]

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,6 +1,6 @@
 module TopicsHelper
   def avatar_for(user, size=32)
-    image_tag user.avatar_url(size), :class => 'photo'
+    "<img src=\"#{user.avatar_url(size)}\" class=\"photo\" />"
   end
 
   def current_user
