e42435d2d2553fc87a3d5a197520a9c108275125
Erlend Simonsen <admin@clockingit.com>
2007-09-28 14:53:53 +0000

    [ClockingIT @ Further printing fixes for the tasks list]

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -11,5 +11,35 @@ module TasksHelper
 
   end
 
+  def print_title
+    filters = []
+    title = "<div style=\"float:left\">"
+    if session[:filter_status].to_i >= 0
+      title << Task.status_types[session[:filter_status].to_i] + " tasks ["
+    else
+      title << "Tasks ["
+    end
+
+    if session[:filter_customer].to_i > 0
+      filters << Customer.find(session[:filter_customer].to_i).name
+    end
+
+    if session[:filter_project].to_i > 0
+      filters << Project.find(session[:filter_project].to_i).name
+    end
+
+    if session[:filter_user].to_i > 0
+      filters << User.find(session[:filter_user].to_i).name
+    end
+
+    filters = session[:user].company.name if filters.empty?
+
+    title << filters.join(' / ')
+
+    title << "]</div><div style=\"float:right\">#{tz.now.strftime("#{session[:user].time_format} #{session[:user].date_format}")}</div><div style=\"clear:both\"></div>"
+
+    "<h3>#{title}</h3>"
+
+  end
 
 end
