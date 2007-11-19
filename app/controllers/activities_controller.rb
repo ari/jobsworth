7843fbe1dda697ccfb5c91a857d2802d50795063
Erlend Simonsen <admin@clockingit.com>
2007-11-19 10:46:22 +0000

    [ClockingIT @ Collapse / Expand project milestones]

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -85,6 +85,11 @@ class ActivitiesController < ApplicationController
     session[:collapse_projects][params[:id]] = 1 - session[:collapse_projects][params[:id]].to_i
   end
 
+  def toggle_display_milestones
+    session[:collapse_milestones] ||= {}
+    session[:collapse_milestones][params[:id]] = 1 - session[:collapse_milestones][params[:id]].to_i
+  end
+
   def toggle_menu
     session[:collapse_menu] ||= 0
     session[:collapse_menu] = 1 - session[:collapse_menu].to_i
