b47a8d59230c64b22c2106c921cdb943a710a418
Erlend Simonsen <admin@clockingit.com>
2007-11-21 21:34:22 +0000

    [ClockingIT @ Stop RJS debugging, as it really complicates escaping for chat]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ config.breakpoint_server = true
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
 config.action_view.cache_template_extensions         = false
-config.action_view.debug_rjs                         = true
+config.action_view.debug_rjs                         = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
