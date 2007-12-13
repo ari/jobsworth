d45904c275ee6482df2985c343c43307aaeb55d2
Erlend Simonsen <admin@clockingit.com>
2007-12-13 11:48:23 +0100

    Use separate hash for timer window auto-login
    As you might send the RSS/iCal hash to other people, use
    a separate one for actually logging in automatically.

---
+++ db/migrate/094_add_autologin_hash.rb
@@ -0,0 +1,19 @@
+class AddAutologinHash < ActiveRecord::Migration
+  def self.up
+    add_column :users, :autologin, :string
+
+    say_with_time("Generating autologin hashes..") do
+      User.find(:all).each do |u|
+        u.autologin = Digest::MD5.hexdigest( rand(100000000).to_s + Time.now.to_s)
+        u.save
+      end
+    end
+
+    add_index :users, :autologin
+  end
+
+  def self.down
+    remove_index :users, :autologin
+    remove_column :users, :autologin
+  end
+end
