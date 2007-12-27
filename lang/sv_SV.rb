d257b77170f37b2813d5c39c96e5b10b2461819e
Erlend Simonsen <admin@clockingit.com>
2007-12-27 23:57:07 +0100

    Initial Swedish translation

---
+++ lang/sv_SV.rb
@@ -0,0 +1,470 @@
+Localization.define('sv_SV') do |l|
+
+  # Main menu
+  l.store "Overview", "Överblick"
+  l.store "Tutorial", "Introduktion"
+  l.store "Browse", "Bläddra"
+  l.store "Timeline", "Tidslinje"
+  l.store "Files", "Filer"
+  l.store "Reports", "Rapporter"
+  l.store "Schedule", "Kalender"
+  l.store "New Task", "Ny Uppgift"
+  l.store "Preferences", "Inställningar"
+  l.store "Log Out", "Logga ut"
+  l.store "Clients", "Kunder"
+  l.store "Client", "Kund"
+  l.store 'Search', 'Sök'
+  l.store 'Users', 'Användare'
+  l.store 'User', 'Användare'
+
+  # Main layout
+  l.store 'Hide', 'Dölj'
+  l.store 'Views', 'Snabbvy'
+  l.store 'Open Tasks', 'Alla Uppgifter'
+  l.store 'My Open Tasks', 'Mina Uppgifter'
+  l.store 'My In Progress Tasks', 'Mina Pågående Uppgifter'
+  l.store 'Unassigned Tasks', 'Uppgifter utan ansvarig'
+  l.store 'Shared', 'Delade'
+  l.store 'Edit', 'Redigera'
+  l.store 'New', 'Ny'
+  l.store 'Chat', 'Chat'
+  l.store 'Notes', 'Anteckningar'
+  l.store 'Feedback? Suggestions? Ideas? Bugs?', 'Återkoppling? Förslag? Idéer? Fel?'
+  l.store 'Let me know', 'Berätta för oss'
+  l.store 'worked today', 'jobbat idag'
+  l.store 'Online', 'Påloggad'
+  l.store 'Done working on <b>%s</b> for now.', 'Arbetat klart med <b>%s</b> för tillfället.' # %s = @task.name
+  l.store 'ago', 'sedan' # X days ago
+
+  # Application Helper
+  l.store 'today','idag'
+  l.store 'tomorrow', 'imorgon'
+  l.store '%d day', ['en dag', '%d dagar']
+  l.store '%d week', ['en vecka', '%d veckor']
+  l.store '%d month', ['en månad', '%d månader']
+  l.store 'yesterday', 'igår'
+  l.store '%d day ago', ['en dag sedan', '%d dagar sedan']
+  l.store '%d week ago', ['en vecka sedan', '%d veckor sedan']
+  l.store '%d month ago', ['en månad sedan', '%d månader sedan']
+
+  # DateHelper
+  l.store 'less than a minute', 'mindre än en minut'
+  l.store '%d minute', ['en minut', '%d minuter']
+  l.store 'less than %d seconds', 'mindre än %d sekunder'
+  l.store 'half a minute', 'en halv minut'
+  l.store 'about %d hour', ['ungefär en timme', 'ungefär %d timmar']
+
+  # Activities
+  l.store 'Top Tasks', 'Viktigaste Uppgifter'
+  l.store 'Newest Tasks', 'Nyaste Uppgifter'
+  l.store 'Recent Activities', 'Senaste aktiviteter'
+  l.store 'Projects', 'Projekt'
+  l.store 'Overall Progress', 'Övergripande Utveckling'
+  l.store '%d completed milestone', ['en färdig milstolpe', '%d färdiga milstolpar']
+  l.store '%d completed project', ['ett färdigt projekt', '%d färdiga projekt']
+  l.store 'Edit project <b>%s</b>', 'Redigera projektet <b>%s</b>'
+  l.store 'Edit milestone <b>%s</b>', 'Redigera milstolpe <b>%s</b>'
+
+  # Tasks
+  l.store 'Tasks', 'Uppgifter'
+  l.store '[Any Client]', '[Alla Kunder]'
+  l.store '[Any Project]', '[Alla Projekt]'
+  l.store '[Any User]', '[Alle Användare]'
+  l.store '[Any Milestone]', '[Alla Milstolpar]'
+  l.store '[Any Status]', '[Alla Statusar]'
+  l.store '[Unassigned]','[Utan Ansvarig]'
+  l.store 'Open', 'Öppna'
+  l.store 'In Progress', 'Pågående'
+  l.store 'Closed', 'Stängda'
+  l.store 'Won\'t Fix', 'Kommer ej att åtgärdas'
+  l.store 'Invalid', 'Ogiltig'
+  l.store 'Duplicate', 'Dublett'
+  l.store 'Archived', 'Arkiverad'
+  l.store 'Group Tags', 'Grupptaggar'
+  l.store 'Save as View', 'Spara som snabbvy'
+  l.store 'Tags', 'Taggar'
+  l.store '[All Tags]', '[Alla Taggar]'
+  l.store 'Close <b>%s</b>', 'Stäng <b>%s</b>'
+  l.store "Stop working on <b>%s</b>.", 'Sluta arbeta med <b>%s</b>.'
+  l.store "Start working on <b>%s</b>. Click again when done.", 'Börja jobba med <b>%s</b>. Klicka igen för att avsluta.'
+  l.store 'No one', 'Ingen'
+  l.store "Revert <b>%s</b> to not completed status.", 'Ställ om <b>%s</b> status till ej färdig'
+  l.store "Cancel working on <b>%s</b>.", 'Avbryt arbetet med <b>%s</b>.'
+  l.store "Move <b>%s</b> to the Archive.", 'Flytta <b>%s</b> till Arkivet.'
+  l.store "Restore <b>%s</b> from the Archive.", 'Återställ <b>%s</b> från Arkivet.'
+  l.store 'Information', 'Information'
+  l.store 'Summary', 'Sammanfattning'
+  l.store 'Description', 'Beskrivning'
+  l.store 'Comment', 'Kommentar'
+  l.store 'Attach file', 'Bifoga fil'
+  l.store 'Target', 'Mål'
+  l.store 'Project', 'Projekt'
+  l.store 'Milestone', 'Milstolpe'
+  l.store '[None]', '[Ingen]'
+  l.store 'Assigned To', 'Ansvarig'
+  l.store 'Requested By', 'Förfrågad av'
+  l.store 'Attributes', 'Attribut'
+
+  l.store 'Type', 'Typ'
+  l.store 'Priority', 'Prioritet'
+  l.store 'Severity', 'Etikett'
+  l.store 'Time Estimate', 'Beräknad Tidsåtgång'
+  l.store 'Due Date', 'Deadline'
+  l.store 'Show Calendar', 'Visa Kalender'
+  l.store 'Notification', 'Meddelande'
+  l.store "Additional people to be notified on task changes<br />in addition to creator and asignee.<br/><br/>Ctrl-click to toggle.", 'Andra personer som skall meddelas vid förändringar i uppgiften<br/>förutom skapare och ansvarig<br/><br/>Ctrl-klick för att välga.'
+  l.store 'To:', 'Till:'
+  l.store '[Delete]', '[Radera]'
+  l.store 'Really delete %s?', 'Är du säker på att du vill radera %s?'
+  l.store 'New Task', 'Ny Uppgift'
+  l.store 'Create', 'Skapa'
+  l.store 'Send notification emails', 'Skicka meddelande via epost'
+  l.store 'Created', 'Skapad'
+  l.store 'by', 'av' # Created by
+  l.store 'Last Updated', 'Senaste uppdatering'
+  l.store 'Save', 'Spara'
+  l.store 'and', 'och' # Save and ...
+
+  l.store "Leave Open",'Lämmna Öppen'
+  l.store "Revert to Open",'Återgå till Öppen status'
+  l.store "Set in Progress",'Ställ om till Pågående status'
+  l.store "Leave as in Progress",'Lämna som Pågående'
+  l.store "Close",'Stäng'
+  l.store "Leave Closed",'Lämna som Stängd'
+  l.store "Set as Won't Fix",'Ställ om till Kommer ej att åtgärdas'
+  l.store "Leave as Won't Fix",'Lämna som Kommer ej att åtgärdas'
+  l.store "Set as Invalid",'Ställ in som Ogiltig'
+  l.store "Leave as Invalid",'Lämna som Ogiltig'
+  l.store "Set as Duplicate",'Ställ in som Dublett'
+  l.store "Leave as Duplicate",'Lämna som Dublett'
+  l.store 'History', 'Historik'
+  l.store 'Edit Log Entry', 'Redigera logg'
+  l.store 'Delete Log Entry', 'Radera logg'
+  l.store 'Really delete this log entry?', 'Är du säker på att du vill radera logg?'
+
+  l.store 'Task', 'Uppgift'
+  l.store 'New Feature', 'Ny Funktion'
+  l.store 'Defect', 'Fel'
+  l.store 'Improvement', 'Förbättring'
+  l.store 'Critical', 'Kritisk'
+  l.store 'Urgent', 'Brådskande'
+  l.store 'High', 'Hög'
+  l.store 'Normal', 'Normal'
+  l.store 'Low', 'Låg'
+  l.store 'Lowest', 'Lägsta'
+  l.store 'Blocker', 'Blockera'
+  l.store 'Major', 'Större'
+  l.store 'Minor', 'Liten'
+  l.store 'Trivial', 'Trivial'
+
+  l.store 'Start', 'Start'
+  l.store 'Duration Worked', 'Arbetad tid'
+
+  # Timeline
+  l.store '[All Time]', '[All Tid]'
+  l.store 'This Week', 'Denna Vecka'
+  l.store 'Last Week', 'Förra veckan'
+  l.store 'This Month', 'Denna Månad'
+  l.store 'Last Month', 'Förra Månaden'
+  l.store 'This Year', 'Detta År'
+  l.store 'Last Year', 'Förra Året'
+  l.store '[Any Type]', '[Alla Typer]'
+  l.store 'Work Log', 'Arbetslogg'
+  l.store 'Status Change', 'Statusförändring'
+  l.store 'Modified', 'Förändrad'
+  l.store '[Prev]', '[Föregående]' # [Prev] 100 of 2000 entries [Next]
+  l.store '[Next]', '[Nästa]' # [Prev] 100 of 2000 entries [Next]
+  l.store 'of', 'av' # 100 of 2000 entries
+  l.store 'entries..', 'poster..' # 100 of 2000 entries
+
+  # Project Files
+  l.store 'Download', 'Ladda hem'
+  l.store 'Delete', 'Radera'
+  l.store '[New File]', '[Ny Fil]'
+  l.store 'File', ['Fil', 'Filer']
+  l.store 'Upload New File', 'Ladda Upp Ny Fil'
+  l.store 'Name', 'Namn'
+  l.store 'Upload', 'Ladda Upp'
+
+  # Reports
+  l.store 'Download CSV file of this report', 'Ladda ned denna rapports CSV-fil.'
+  l.store 'Total', 'Totalt'
+  l.store 'Report Configuration', 'Konfigurera Rapport'
+  l.store 'Report Type', 'Rapporttyp'
+  l.store 'Pivot', 'Pivot'
+  l.store 'Audit', 'Granska'
+  l.store 'Time sheet', 'Fullständig'
+  l.store 'Time Range', 'Tidsomfång'
+  l.store 'Custom', 'Anpassad'
+  l.store 'Rows', 'Rader'
+  l.store 'Columns', 'Kolumner'
+  l.store "Milestones", 'Milstolpar'
+  l.store "Date", 'Datum'
+  l.store 'Task Status', 'Status för Uppgift'
+  l.store "Task Type", 'Typ av Uppgift'
+  l.store "Task Priority", 'Prioritet för Uppgift'
+  l.store "Task Severity", 'Etikett för Uppgift'
+  l.store 'From', 'Från' # From Date
+  l.store 'To', 'Till' # To Date
+  l.store 'Sub-totals', 'Delsummor'
+  l.store 'Filter', 'Filtrering'
+  l.store 'Advanced Options', 'Avancerade inställningar'
+  l.store 'Status', 'Status'
+  l.store 'Run Report', 'Generera Rapport'
+
+  # Schedule
+
+  # Search
+  l.store 'Search Results', 'Sökresultat'
+  l.store 'Activities', 'Aktiviteter'
+
+  # Project list
+  l.store 'Read', 'Läs'
+  l.store 'Work', 'Arbeta'
+  l.store 'Assign', 'Tildela'
+  l.store 'Prioritize', 'Prioritera'
+  l.store 'Grant', 'Ge tillgång'
+  l.store "Remove all access for <b>%s</b>?", 'Ta bort all tillgång för <b>%s</b>?'
+  l.store "Grant %s access for <b>%s</b>?", 'Ge tilgang till %s för <b>%s</b>?'
+  l.store "Can't remove <b>yourself</b> or the <b>project creator</b>!", 'Du kan inte ta bort <b>dig själv</b> eller <b>projekt skaparen</b>!'
+  l.store "Grant access to <b>%s</b>?", 'Ge tillgång till <b>%s</b>?'
+  l.store 'Edit Project', 'Redigera Projekt'
+  l.store 'Delete Project', 'Radeera Projekt'
+  l.store 'Complete Project', 'Avsluta Projekt'
+  l.store 'New Milestone', 'Ny Milstolpe'
+  l.store 'Access To Project', 'Tillgång till Projektet'
+  l.store 'Completed', 'Avslutat'
+  l.store 'Completed Projects', 'Avslutade Projekt'
+  l.store 'Revert', 'Återställ'
+  l.store 'Really revert %s?', 'Är du säker på att du vill Återställa %s?'
+
+  # Milestones
+  l.store 'Owner', 'Ansvarig'
+  l.store 'Edit Milestone', 'Redigera Milstolpe'
+  l.store 'Delete Milestone', 'Radera Milstolpe'
+  l.store 'Complete Milestone', 'Avsluta Milstolpe'
+  l.store 'Completed Milestones', 'Avslutade Milstolpar'
+
+  # Users
+  l.store 'Email', 'Epost'
+  l.store 'Last Login', 'Senaste Inloggning'
+  l.store 'Offline', 'Offline'
+  l.store 'Are your sure?', 'är du säker?'
+  l.store 'Company', 'Företag'
+  l.store '[New User]', '[Ny Användare]'
+  l.store '[Previous page]', '[Föregående sida]'
+  l.store '[Next page]', '[Nästa sida]'
+  l.store 'Edit User', 'Redigera Användaree'
+
+  l.store 'Options', 'Val'
+  l.store 'Location', 'Stad'
+  l.store 'Administrator', 'Administratör'
+  l.store 'Track Time', 'Logga Tid'
+  l.store 'Use External Clients', 'Använd Externa Klienter'
+  l.store 'Show Calendar', 'Visa kalender'
+  l.store 'Show Tooltips', 'Visa Tips'
+  l.store 'Send Notifications', 'Skicka meddelanden'
+  l.store 'Receive Notifications', 'Ta emot meddelanden'
+
+  l.store 'User Information', 'Användarinformation'
+  l.store 'Username', 'Användarnamn'
+  l.store 'Password', 'Lösenord'
+
+  # Preferences
+  l.store 'Preferences', 'Inställningar'
+  l.store 'Language', 'Språk'
+  l.store 'Time Format', 'Tidsformat'
+  l.store 'Date Format', 'Datumformat'
+  l.store 'Custom Logo', 'Egen Logo'
+  l.store 'Current logo', 'Aktiv Logo'
+  l.store 'New logo', 'Ny logo'
+  l.store "(250x50px should look good. The logo will be shown up top instead of the ClockingIT one, and on your login page.)", "(250x50px är den optimala storleken. Logon kommer att visas i övre vänstra hörnet, istället för ClockingIT logon, och även på loginsidan.)"
+
+  # Notes / Pages
+  l.store 'Body', 'Innehåll'
+  l.store 'Preview', 'Förhandsvisning'
+  l.store 'New Note', 'Ny Anteckning'
+  l.store 'Edit Note', 'Redigera Anteckning'
+
+  # Views
+  l.store 'New View', 'Ny Snabbvy'
+  l.store 'Edit View', 'Redigera Snabbvy'
+  l.store 'Delete View', 'Radera Snabbvy'
+  l.store '[Active User]', '[Aktiv Användare]'
+  l.store 'Shared', 'Delad'
+
+  # Clients
+  l.store 'Contact', 'Kontakt'
+  l.store 'New Client', 'Ny Kund'
+  l.store 'Contact email', 'Epost'
+  l.store 'Contact name', 'Namn'
+  l.store 'Client CSS', 'Kundens CSS'
+
+  # Activities Controller
+  l.store 'Tutorial completed. It will no longer be shown in the menu.', 'Introduktion färdig. Den kommer inte visas i menyn längre.'
+  l.store 'Tutorial hidden. It will no longer be shown in the menu.', 'Introduktion färdig. Den kommer inte visas i menyn längre.'
+
+  # Customers Controller
+  l.store 'Client was successfully created.', 'Ny kund skapades utan problem.'
+  l.store 'Client was successfully updated.', 'Kundinfo uppdaterades utan problem.'
+  l.store 'Please delete all projects for %s before deleting it.', 'Radera alla projekt för %s innan du raderar kund.'
+  l.store "You can't delete your own company.", 'Du kan inte radera ditt eget företag.'
+  l.store 'CSS successfully uploaded.', 'CSS lastet opp.'
+  l.store 'Logo successfully uploaded.', 'Logo lastet opp.'
+
+  # Milestones Controller
+  l.store 'Milestone was successfully created.', 'Milstolpe skapades utan problem.'
+  l.store 'Milestone was successfully updated.', 'Milstolpe uppdaterades utan problem.'
+  l.store '%s / %s completed.', '%s / %s avslutad.' # Project name / Milestone name completed.
+  l.store '%s / %s reverted.', '%s / %s återställd.' # Project name / Milestone name reverted.
+
+  # Pages / Notes Controller
+  l.store 'Note was successfully created.', 'Anteckning skapades utan problem.'
+  l.store 'Note was successfully updated.', 'Anteckning uppdaterades utan problem.'
+
+  # Project Files Controller
+  l.store 'No file selected for upload.', 'Du har inte valt någon fil att ladda upp.'
+  l.store 'File too big.', 'Filen är för stor.'
+  l.store 'File successfully uploaded.', 'Fil laddades upp utan problem.'
+
+  # Projects Controller
+  l.store 'Project was successfully created.', 'Projekt skapades utan problem.'
+  l.store 'Project was successfully created. Add users who need access to this project.', 'Projekt skapades utan problem. Ge tillgång till användare som behöver åtkomst till projektet.'
+  l.store 'Project was successfully updated.', 'Projekt uppdaterades utan problem.'
+  l.store 'Project was deleted.', 'Projekt raderades.'
+  l.store '%s completed.', '%s avslutat.'
+  l.store '%s reverted.', '%s återställt.'
+
+  # Reports Controller
+  l.store "Empty report, log more work!", 'Rapporten är tom, du måste logga arbetstid innan du kan ta ut en rapport.'
+
+  # Tasks Controller
+  l.store "You need to create a project to hold your tasks, or get access to create tasks in an existing project...", 'Du måste skapa ett projekt för att samla dina uppgifter i eller få tillåtelse att skapa uppgifter i ett befintligt projekt...'
+  l.store 'Invalid due date ignored.', 'Ogiltig deadline ignorerad.'
+  l.store 'Task was successfully created.', 'Uppgift skapades utan problem.'
+  l.store 'Task was successfully updated.', 'Uppgift uppdaterades utan problem.'
+  l.store 'Log entry saved...', 'Arbetslogg sparad...'
+  l.store "Unable to save log entry...", 'Arbetslogg kunde ej sparas...'
+  l.store "Log entry already saved from another browser instance.", 'Arbetslogg är redan sparad från en annan webbläsarinstans.'
+  l.store 'Log entry deleted...', 'Arbetslogg raderad...'
+
+  # Users Controller
+  l.store 'User was successfully created. Remember to give this user access to needed projects.', 'Användare skapades utan problem. Glöm inte att tilldela användaren tillgång till de projekt som hon/han behöver deltaga i.'
+  l.store "Error sending creation email. Account still created.", 'Det gick inte att skicka epost. Kontot skapades dock utan problem.'
+  l.store 'User was successfully updated.', 'Användare uppdaterades utan problem.'
+  l.store 'Preferences successfully updated.', 'Innställningar uppdaterades utan problem.'
+
+  # Views Controller
+  l.store "View '%s' was successfully created.", "Snabbvy '%s' skapades utan problem."
+  l.store "View '%s' was successfully updated.", "Snabbvy '%s' uppdaterad utan problem."
+  l.store "View '%s' was deleted.", "Snabbvy '%s' raderad."
+
+  # Wiki
+  l.store 'Quick Reference', 'Snabb Översikt'
+  l.store 'Full Reference', 'Komplett Översikt'
+  l.store 'or', 'eller'
+  l.store 'Under revision by', 'Under revidering av'
+  l.store 'Revision', 'Revision'
+  l.store 'Linked from', 'Länkad från'
+
+  # Reports
+  l.store 'Today', 'Idag'
+  l.store 'Week', 'Vecka'
+
+  # Dates
+  l.store 'January', 'Januari'
+  l.store 'February', 'Februari'
+  l.store 'March', 'Mars'
+  l.store 'April', 'April'
+  l.store 'May', 'Maj'
+  l.store 'June', 'Juni'
+  l.store 'July', 'Juli'
+  l.store 'August', 'Augusti'
+  l.store 'September', 'September'
+  l.store 'October', 'Oktober'
+  l.store 'November', 'November'
+  l.store 'December', 'December'
+
+  l.store 'Jan', 'Jan'
+  l.store 'Feb', 'Feb'
+  l.store 'Mar', 'Mar'
+  l.store 'Apr', 'Apr'
+  l.store 'May', 'Maj'
+  l.store 'Jun', 'Jun'
+  l.store 'Jul', 'Jul'
+  l.store 'Aug', 'Aug'
+  l.store 'Sep', 'Sep'
+  l.store 'Oct', 'Okt'
+  l.store 'Nov', 'Nov'
+  l.store 'Dec', 'Dec'
+
+  l.store 'Sunday', 'Söndag'
+  l.store 'Monday', 'Måndag'
+  l.store 'Tuesday', 'Tisdag'
+  l.store 'Wednesday', 'Onsdag'
+  l.store 'Thursday', 'Torsdag'
+  l.store 'Friday', 'Fredag'
+  l.store 'Saturday', 'Lördag'
+
+  l.store 'Sun', 'Sön'
+  l.store 'Mon', 'Mån'
+  l.store 'Tue', 'Tis'
+  l.store 'Wed', 'Ons'
+  l.store 'Thu', 'Tor'
+  l.store 'Fri', 'Fre'
+  l.store 'Sat', 'Lör'
+
+  # worked_nice
+  l.store '[wdhm]', '[vdtm]'
+  l.store 'w', 'v'
+  l.store 'd', 'd'
+  l.store 'h', 't'
+  l.store 'm', 'm'
+
+  # Preferences
+  l.store 'Duration Format', 'Tidsformat för loggning'
+  l.store 'Workday Length', 'Arbetsdagens längd'
+
+  # Tasks filter
+  l.store '[Without Milestone]', '[Utan Milstolpe]'
+
+  # Task tooltip
+  l.store 'Progress', 'Fortskridning'
+
+  # User Permissions
+  l.store 'All', 'Alla'
+
+  # Reports filter
+  l.store '[Any Priority]', '[Alla Prioriteter]'
+  l.store '[Any Severity]', '[Alla Etiketter]'
+
+  # Preferences
+  l.store '1w 2d 3h 4m', '1v 2d 3t 4m'
+  l.store '1w2d3h4m', '1v2d3t4m'
+
+  # Task
+  l.store "Attachments", "Bilagor"
+  l.store "Dependencies", "Beroende av"
+  l.store 'Add another dependency', "Lägg till beroendeförhållande"
+  l.store 'Remove dependency', "Radera beroendeförhållande"
+  l.store "every", 'varje'
+  l.store "[Any Task]", "[Alla Uppgifter]"
+
+  l.store 'day', 'dag' # every day
+  l.store 'days', 'dag' # every 14 days
+  l.store 'last', 'sista' # every last thursday
+
+  l.store 'Hide Waiting Tasks', 'Dölj Väntande Uppgifter'
+  l.store 'Signup Message', 'Personligt meddelande'
+  l.store 'The message will be included in the signup email.', 'Meddelandet skickas tillsammans med registreringsuppgifterna.'
+  l.store 'Depends on', 'Beroende av'
+
+  # Activities
+  l.store 'Subscribe to the recent activities RSS feed', 'Prenumerera på de senaste händelserna via RSS'
+
+  # Project Files
+  l.store '%d folder', ['%d mapp', '%d mappar']
+  l.store '%d file', ['%d fil', '%d filer']
+
+end
+
