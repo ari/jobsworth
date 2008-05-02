805adae55c1d6b89c244e6b7545cd53cf419308d
Erlend Simonsen <admin@clockingit.com>
2008-05-02 17:16:51 +0200

    Test for checking empty username/password

--- test/functional/login_controller_test.rb
+++ test/functional/login_controller_test.rb
@@ -1,4 +1,5 @@
 context "A user" do
+  use_controller LoginController
   fixtures :users, :companies, :customers
   
   setup do
@@ -8,7 +9,6 @@ context "A user" do
   
   specify "is required to log in" do 
     use_controller ActivitiesController
-
     get :list
     response.should.redirect :controller => 'login', :action => 'login'
   end
@@ -28,6 +28,11 @@ context "A user" do
     should.redirect
   end 
   
+  specify "should not login without username and password" do
+    post :validate, :user => { 'username' => '', 'password' => '' }
+    should.redirect_to :controller => 'login', :action => 'login'
+  end 
+
   specify "should be able to log in" do
     post :validate, :user => { 'username' => 'test', 'password' => 'password' }
     should.redirect_to :controller => 'activities', :action => 'list'
