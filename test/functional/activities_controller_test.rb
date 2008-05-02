5adcce771550e9088bcd1d8d11dc27aa9fdbe4de
Erlend Simonsen <admin@clockingit.com>
2008-05-02 10:23:41 +0200

    Update tests to handle the widgetized way of doing things

--- test/functional/activities_controller_test.rb
+++ test/functional/activities_controller_test.rb
@@ -10,7 +10,7 @@ context "Activities" do
   
   specify "/index should render :success" do
     get :index
-    assigns(:tz).should.equal Timezone.get('Europe/Oslo')
+    assigns(:current_user).tz.should.equal Timezone.get('Europe/Oslo')
     assigns(:current_user).should.equal User.find(1)
     status.should.be :success
   end
@@ -18,16 +18,7 @@ context "Activities" do
   specify "/list should render :success" do 
     get :list
     status.should.be :success
-    template.should.be 'activities/list'
-    
-    assigns(:projects).should.not.be.nil
-    assigns(:projects).size.should.be > 0
-    
-    assigns(:tasks).should.not.be.nil
-    assigns(:tasks).size.should.be > 0
-    
-    assigns(:tasks).should.not.equal assigns(:new_tasks)
-    
+    template.should.be 'activities/list'    
   end
   
 end
