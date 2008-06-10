9b53d7ae98bba1a805c4b6925302ec639cbb1837
Erlend Simonsen <admin@clockingit.com>
2008-06-10 22:50:16 +0200

    Allow renaming of company name

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,7 +12,19 @@ class CompaniesController < ApplicationController
 
   def update
     @company = current_user.company
+
+    @internal = @company.internal_customer
+
+    if @internal.nil?
+	flash['notice'] = 'Unable to find internal customer.'
+        render :action => 'edit'
+	return
+    end
+
     if @company.update_attributes(params[:company])
+      @internal.name = @company.name
+      @internal.save
+
       flash['notice'] = _('Company settings updated')
       redirect_from_last
     else
