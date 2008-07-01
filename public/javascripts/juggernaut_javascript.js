cfda08276c0c34e0a5393a209c0ad704d0e07829
Erlend Simonsen <admin@clockingit.com>
2008-07-01 20:55:38 +0200

    Reimplementation of the Flash applet, moving away from fscommand and using ExternalInterface instead.
    Also, added sounds on certain push messages, and removed the
    base64 encoding, as it's all json encoded anyways.
    
    New user option (Enable Sounds) added, requiring a
    rake db:migrate
    
    As the javascript has changed, and I added swfobject
    to load the Flash applet properly, a
    rake asset:packager:build_all
    is required as well.

--- public/javascripts/juggernaut_javascript.js
+++ public/javascripts/juggernaut_javascript.js
@@ -21,124 +21,84 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+var juggernaut;
 
-var InternetExplorer = navigator.appName.indexOf("Microsoft") != -1;
-var connected = false;
+var Juggernaut = Class.create({
+  isConnected: false,
 
-function myFlash_DoFSCommand(command, args)                {
-//  var myFlashObj = InternetExplorer ? myFlash : document.myFlash;
-  flashData (args);
-}
+  initialize: function(options) {
+    this.options = options;
 
-function juggernautInit() {
-if (navigator.appName && navigator.appName.indexOf("Microsoft") != -1 &&
-  navigator.userAgent.indexOf("Windows") != -1 && navigator.userAgent.indexOf("Windows 3.1") == -1) {
-  document.write('<SCRIPT LANGUAGE=VBScript\> \n');
-  document.write('on error resume next \n');
-  document.write('Sub myFlash_FSCommand(ByVal command, ByVal args)\n');
-  document.write(' call myFlash_DoFSCommand(command, args)\n');
-  document.write('end sub\n');
-  document.write('</SCRIPT\> \n');
-}
-}
+    Event.observe(window, 'load', function() {
+		    juggernaut = this;
+		    this.setup();
+		  }.bind(this)
+		 );
+  },
 
-function flashData(data) {
-//alert(decode64(data)); // For debugging
-eval(utf8to16(decode64(data)));
-}
+  initialized: function() {
+    this.connect();
+  },
 
-function flashConnected() {
-  connected = true;
-//alert('You have been connected');
-}
+  swf: function () {
+    return $('flash_server');
+  },
 
-function flashErrorConnecting() {
-  Element.update('flash_message', 'Unable to connect to push server...');
-  Element.show('flash');
-}
+  connect: function() {
+    this.swf().connect(this.options.host, this.options.port);
+  },
 
-function flashConnectionLost(){
-  Element.update('flash_message', 'Connection to push server lost. Please reload the page...');
-  Element.show('flash');
-}
+  connected: function() {
+    this.isConnected = true;
+    this.sendData( $H({"broadcast":0,"channels":this.options.channels}).toJSON() );
+  },
 
-// This code was written by Tyler Akins and has been placed in the
-// public domain.  It would be nice if you left this header intact.
-// Base64 code from Tyler Akins -- http://rumkin.com
+  sendData: function(data) {
+    this.swf().sendData(escape(data));
+  },
+
+  errorConnecting: function() {
+    Element.update('flash_message', 'Unable to connect to push server...');
+    Element.show('flash');
+  },
+
+  disconnected: function () {
+    Element.update('flash_message', 'Connection to push server lost. Please reload the page...');
+    Element.show('flash');
+  },
+
+  receiveData: function (data) {
+    var msg = unescape(data);
+    eval(msg);
+  },
+
+  playSound: function(sound) {
+    if(this.options.sounds) {
+      this.swf().playSound(sound);
+    }
+  },
+
+  setup: function() {
+
+    var attributes = {
+      id: "flash_server",
+      name: "flash_server"
+    };
+
+    this.element = new Element('div', {id:'socket_server'});
+    $(document.body).insert({bottom: this.element });
+
+    swfobject.embedSWF("/socket_server.swf", "socket_server", "1", "1", "8,0,0,0", false, {}, {allowScriptAccess:"always"}, attributes);
+  }
+
+
+});
 
 function checkConnection() {
-  if( !connected ) {
+  if( !juggernaut.isConnected ) {
     Element.update('flash_message', 'Unable to connect to push server, make sure flash is enabled for this site and SSL traffic is allowed...<br />You won\'t be able to see what others are saying.');
     Element.show('flash');
   }
 }
 
 
-function decode64(input) {
-
-var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-
-   var output = "";
-   var chr1, chr2, chr3;
-   var enc1, enc2, enc3, enc4;
-   var i = 0;
-
-   // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-   input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-   do {
-      enc1 = keyStr.indexOf(input.charAt(i++));
-      enc2 = keyStr.indexOf(input.charAt(i++));
-      enc3 = keyStr.indexOf(input.charAt(i++));
-      enc4 = keyStr.indexOf(input.charAt(i++));
-
-      chr1 = (enc1 << 2) | (enc2 >> 4);
-      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-      chr3 = ((enc3 & 3) << 6) | enc4;
-
-      output = output + String.fromCharCode(chr1);
-
-      if (enc3 != 64) {
-         output = output + String.fromCharCode(chr2);
-      }
-      if (enc4 != 64) {
-         output = output + String.fromCharCode(chr3);
-      }
-   } while (i < input.length);
-
-   return output;
-}
-
-function utf8to16(str) {
-    var out, i, len, c;
-    var char2, char3;
-
-    out = "";
-    len = str.length;
-    i = 0;
-    while(i < len) {
-        c = str.charCodeAt(i++);
-        switch(c >> 4)
-        {
-          case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-            // 0xxxxxxx
-            out += str.charAt(i-1);
-            break;
-          case 12: case 13:
-            // 110x xxxx   10xx xxxx
-            char2 = str.charCodeAt(i++);
-            out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-            break;
-          case 14:
-            // 1110 xxxx  10xx xxxx  10xx xxxx
-            char2 = str.charCodeAt(i++);
-            char3 = str.charCodeAt(i++);
-            out += String.fromCharCode(((c & 0x0F) << 12) |
-                                           ((char2 & 0x3F) << 6) |
-                                           ((char3 & 0x3F) << 0));
-            break;
-        }
-    }
-
-    return out;
-}
