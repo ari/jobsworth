7a6275c60d35b4cffce49d87b9c9f39e08ce5b64
Erlend Simonsen <admin@clockingit.com>
2008-07-12 18:32:58 +0200

    Truncate task/milestone description in tooltip

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -24,7 +24,7 @@ class Milestone < ActiveRecord::Base
     res << "<tr><th>#{_('Due Date')}</th><td> #{options[:user].tz.utc_to_local(due_at).strftime_localized("%A, %d %B %Y")}</td></tr>" unless self.due_at.nil?
     res << "<tr><th>#{_('Owner')}</th><td> #{self.user.name}</td></tr>" unless self.user.nil?
     res << "<tr><th>#{_('Progress')}</th><td> #{self.completed_tasks.to_i} / #{self.total_tasks.to_i} #{_('Complete')}</td></tr>"
-    res << "<tr><th>#{_('Description')}</th><td class=\"tip_description\">#{self.description.gsub(/\n/, '</td></tr>').gsub(/\"/,'&quot;')}</td></tr>" if( self.description && self.description.strip.length > 0)
+    res << "<tr><th>#{_('Description')}</th><td class=\"tip_description\">#{self.description.slice(0,4096).gsub(/\n/, '</td></tr>').gsub(/\"/,'&quot;')}</td></tr>" if( self.description && self.description.strip.length > 0)
     res << "</table>"
     res.gsub(/\"/,'&quot;')
   end
