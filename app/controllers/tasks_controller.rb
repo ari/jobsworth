11f9205b0b8131fa49503ce21f7b4be3838f4090
Erlend Simonsen <admin@clockingit.com>
2008-08-03 19:40:05 +0200

    Reorder :include elements to hit index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -110,11 +110,7 @@ class TasksController < ApplicationController
     else
       # Looking for tasks based on filters
       @selected_tags = []
-#      @tasks = Task.find(:all, :conditions => [filter + "tasks.company_id = #{current_user.company_id} AND tasks.project_id IN (#{project_ids})"],  :order => " tasks.completed_at IS NOT NULL, tasks.completed_at desc, #{sort}", :include => [ :users, :tags, :work_logs, :milestone, { :project => :customer }, :dependencies, :dependants, :todos ])
-
-#      @tasks = Task.find(:all, :conditions => [filter + "tasks.company_id = #{current_user.company_id} AND tasks.project_id IN (#{project_ids})"],  :order => " tasks.completed_at IS NOT NULL, tasks.completed_at desc, #{sort}", :include => [ :milestone, :dependencies, :dependants, :users, :tags, :todos, :sheets ], :joins => [:project => :customer])
-
-      @tasks = Task.find(:all, :conditions => ["tasks.company_id = #{current_user.company_id} AND tasks.project_id IN (#{project_ids}) AND " + filter],  :order => " tasks.completed_at IS NOT NULL, tasks.completed_at desc, #{sort}", :include => [:milestone, :dependencies, :dependants, :users, :tags, { :project => :customer}, :sheets, :todos])
+      @tasks = Task.find(:all, :conditions => ["tasks.company_id = #{current_user.company_id} AND tasks.project_id IN (#{project_ids}) AND " + filter],  :order => " tasks.completed_at IS NOT NULL, tasks.completed_at desc, #{sort}", :include => [:users, :tags, :sheets, :todos, :dependencies, :dependants, { :project => :customer}, :milestone ])
     end
 
     # Most popular tags, currently unlimited.
