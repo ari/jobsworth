99c41c5f2b0f1e76c4af8bc6286fdd61067cabc0
Erlend Simonsen <erlends@fudgie.localdomain>
2008-10-28 20:22:24 +0100

    Log who completes a todo, and display it in the completed todo row

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,8 @@
 class Todo < ActiveRecord::Base
   belongs_to :company
   belongs_to :task
+  belongs_to :completed_by_user, :class_name => "User", :foreign_key => "completed_by_user_id"
+
 
   acts_as_list :scope => 'task_id = #{task_id} AND completed_at IS NULL'
 
