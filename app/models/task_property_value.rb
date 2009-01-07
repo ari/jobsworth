c52056825075c7b415b7951ded3004804fdd73e6
Brad Wilson <brad@lucky-dip.net>
2009-01-08 07:09:37 +1100

    Added some comments to property models

--- app/models/task_property_value.rb
+++ app/models/task_property_value.rb
@@ -1,3 +1,7 @@
+###
+# A TaskPropertyValue links a task with a particular property
+# and a value for that property.
+###
 class TaskPropertyValue < ActiveRecord::Base
   belongs_to :task
   belongs_to :property
