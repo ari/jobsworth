3522c08792aada85cb91d6dda8e324fddd4f47df
Brad Wilson <brad@lucky-dip.net>
2009-01-16 14:24:02 +1100

    add test on task list

--- test/functional/tasks_controller_test.rb
+++ test/functional/tasks_controller_test.rb
@@ -17,4 +17,20 @@ context "Tasks" do
     status.should.be :success
   end
 
+  specify "/list should render :success" do
+    company = companies("cit")
+
+    # need to create a task to ensure the task partials get rendered
+    task = Task.new(:name => "Test", :project_id => company.projects.last.id)
+    task.company = company
+    task.save!
+
+    get :list
+    status.should.be :success
+
+    # ensure at least 1 task was rendered
+    group = assigns["groups"].first
+    assert group.length > 0
+  end
+
 end
