790c0455915ed6c5d84c4f772b47b558c02670ae
Brad Wilson <brad@lucky-dip.net>
2009-03-09 12:49:15 +1100

    #1056: fixed group by filtering exception

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1599,22 +1599,32 @@ class TasksController < ApplicationController
       projects.each { |p| group_ids[p.full_name] = p.id }
       items = projects.collect(&:full_name).sort
       groups = Task.group_by(tasks, items) { |t,i| t.project.full_name == i }
+
     elsif session[:group_by].to_i == 4 # Milestones
-      filter = ""
-      if session[:filter_milestone].to_i > 0
-        m = Milestone.find(session[:filter_milestone], :conditions => ["project_id IN (#{current_project_ids}) AND completed_at IS NULL"], :order => "due_at, name") rescue nil
-        filter = " AND project_id = #{m.project_id}" if m
-      elsif session[:filter_project].to_i > 0
-        filter = " AND project_id = #{session[:filter_project]}"
-      elsif session[:filter_customer].to_i > 0
-        pids = current_user.company.customers.find(session[:filter_customer]).projects.collect{|p| p.id}.join(',') rescue '0'
-        filter = " AND project_id IN (#{pids})"
-                                                                                           end
-      milestones = Milestone.find(:all, :conditions => ["company_id = ? AND project_id IN (#{current_project_ids})#{filter} AND completed_at IS NULL", current_user.company_id], :order => "due_at, name")
+      tf = TaskFilter.new(self, session)
+      
+      if tf.milestone_ids.any?
+        filter = " AND id in (#{ tf.milestone_ids.join(",") })"
+      elsif tf.project_ids.any?
+        filter = " AND project_id in (#{ tf.project_ids.join(",") })"
+      elsif tf.customer_ids.any?
+        projects = []
+        tf.customer_ids.each { |id| projects += Customer.find(id).projects }
+        projects = projects.map { |p| p.id }
+        filter = " AND project_id IN (#{ projects.join(",") })"
+      end
+
+      conditions = "company_id = #{ current_user.company.id }"
+      conditions += " AND project_id IN (#{current_project_ids})#{filter} "
+      conditions += " AND completed_at IS NULL"
+
+      milestones = Milestone.find(:all, :conditions => conditions, 
+                                  :order => "due_at, name")
       milestones.each { |m| group_ids[m.name + " / " + m.project.name] = m.id }
       group_ids['Unassigned'] = 0
       items = ["Unassigned"] +  milestones.collect{ |m| m.name + " / " + m.project.name }
       groups = Task.group_by(tasks, items) { |t,i| (t.milestone ? (t.milestone.name + " / " + t.project.name) : "Unassigned" ) == i }
+
     elsif session[:group_by].to_i == 5 # Users
       users = current_user.company.users
       users.each { |u| group_ids[u.name] = u.id }
