5016a88a6c13c493de279e67ece8da7d7060783f
Aristedes Maniatis <ari@ish.com.au>
2009-04-26 13:56:29 +1000

    Move a bunch of plugins to gems and define the complete list of gems which are known to work and be required.

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
+# RAILS_GEM_VERSION="2.3.2"
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -40,6 +42,40 @@ Rails::Initializer.run do |config|
 
   # See Rails::Configuration for more options
   config.logger = Logger.new("#{RAILS_ROOT}/log/#{ENV['RAILS_ENV']}.log", 5)
+  
+  config.gem 'splattael-activerecord_base_without_table',	:lib    => 'activerecord_base_without_table',
+															:source => 'http://gems.github.com'
+
+  #config.gem 'rails', :version => '2.3.2'
+  #config.gem 'actionpack', :version => '2.3.2'
+  #config.gem 'actionmailer', :version => '2.3.2'
+  #config.gem 'activerecord', :version => '2.3.2'
+  #config.gem 'activeresource', :version => '2.3.2'
+  #config.gem 'activesupport', :version => '2.3.2'
+          
+  config.gem 'mysql', :version => '2.7'
+  config.gem 'daemons', :version => '1.0.10'
+  config.gem 'eventmachine', :version => '0.12.6'
+  config.gem 'json', :version => '1.1.4'
+  config.gem 'mislav-will_paginate', :version => '2.3.8', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'ferret', :version => '0.11.6'
+  config.gem 'acts_as_ferret', :version => '0.4.3'
+  config.gem 'fastercsv', :version => '1.4.0'
+  config.gem 'icalendar', :version => '1.1.0'
+  config.gem 'tzinfo', :version => '0.3.12'
+  config.gem 'RedCloth', :version => '4.1.9'
+  config.gem 'rmagick', :version => '2.9.1'
+  config.gem 'ZenTest', :version => '4.0.0'
+  config.gem 'hoe', :version => '1.12.1'
+  config.gem 'gchartrb', :version => '0.8', :lib => 'google_chart'
+  config.gem 'test-spec', :version => '0.10.0', :lib => 'test/spec'
+  config.gem 'echoe', :version => '3.1.1'
+  
+  # Juggernaut is installed as a plugin and heavily customised, therefore it cannot be listed here.
+  
+  # Required for development only
+  config.gem 'allison', :version => '2.0.3'
+  config.gem 'markaby', :version => '0.5'
 end
 
 ActionController::Base.session_options[:session_expires]= Time.local(2015,"jan")
