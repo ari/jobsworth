d0a0cacf060f06c5f2c242d3bf8adaffc2042d86
Brad Wilson <brad@lucky-dip.net>
2009-05-05 15:55:39 +1000

    reduce queries for client list on task edit page

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -297,4 +297,26 @@ module TasksHelper
     links = links.sort.join(", ")
     return links
   end
+
+  ###
+  # Returns a list of options to use for the project select tag.
+  ###
+  def options_for_user_projects(task)
+    projects = current_user.projects.find(:all, :include => "customer", :order => "customers.name, projects.name")
+
+    last_customer = nil
+    options = []
+
+    projects.each do |project|
+      if project.customer != last_customer
+        options << [ h(project.customer.name), [] ]
+        last_customer = project.customer
+      end
+
+      options.last[1] << [ project.name, project.id ]
+    end
+
+    return grouped_options_for_select(options, task.project_id)
+  end
+
 end
