5648c52a4160fa627c7936e8dec32ba87c170b6f
Aristedes Maniatis <ari@ish.com.au>
2009-05-14 21:14:27 +1000

    Remove accidentally added colour picker code. I don't really want to bloat the app with another 70k of js just for this...

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,3 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 jQuery.noConflict();
 
 Shadowbox.loadSkin('classic', '/javascripts/shadowbox');
