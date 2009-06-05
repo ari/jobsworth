be9d75ab406764df2aa5a4350feb1794901ef1b5
Brad Wilson <brad@lucky-dip.net>
2009-06-05 18:16:06 +1000

    moved normal task filter to reports page

--- app/helpers/task_filter_helper.rb
+++ app/helpers/task_filter_helper.rb
@@ -4,7 +4,7 @@ module TaskFilterHelper
   # Initialises the instance variables needed to display the task
   # filter partial.
   ###
-  def init_filter_variables
+  def init_filter_variables(locals)
     customer_ids = TaskFilter.filter_ids(session, :filter_customer, TaskFilter::ALL_CUSTOMERS)
     if customer_ids.any?
       @all_projects = current_user.projects.find(:all, :conditions => ["customer_id in (#{ customer_ids.join(",") }) AND completed_at IS NULL"])
@@ -21,6 +21,9 @@ module TaskFilterHelper
       @user_ids = [0] if @user_ids.size == 0
       @users = User.find(:all, :conditions => ["id IN (#{@user_ids.join(',')})"], :order => "name")
     end
+
+    hide_display_options = locals[:hide_display_options]
+    @task_only_option_style = "display: none;" if hide_display_options
   end
 
   ###
@@ -234,6 +237,10 @@ module TaskFilterHelper
     end
 
     return options
+  end
+
+  def show_filter_legend?
+    return controller_name == "reports"
   end
 
 end
