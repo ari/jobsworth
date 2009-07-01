ed88a7dcd4e1d82855f7408105b4561cd9fd5021
Brad Wilson <brad@lucky-dip.net>
2009-07-01 18:17:58 +1000

    move dev only gems to the environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,8 @@ config.action_view.debug_rjs                         = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# Required for development only
+config.gem 'allison'
+config.gem 'markaby'
+config.gem 'fiveruns_tuneup'
