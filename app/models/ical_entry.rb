5676a7e92e67c32f285ab2e3d02bfb9105aa015f
Brad Wilson <brad@lucky-dip.net>
2009-08-10 13:01:48 +1000

    remove task row caching

--- app/models/ical_entry.rb
+++ app/models/ical_entry.rb
@@ -1,6 +1,6 @@
 # Cached generated iCalendar entries, to speed up the feed generation
 
 class IcalEntry < ActiveRecord::Base
-  belongs_to :task, :touch => true
+  belongs_to :task
   belongs_to :work_log
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@
 
 class Notification < ActiveRecord::Base
   belongs_to :user
-  belongs_to :task, :touch => true
+  belongs_to :task
 
   named_scope :unread, :conditions => { :unread => true }
 end
