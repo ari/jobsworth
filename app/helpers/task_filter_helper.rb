cf8bf35580bd90a78c5297645b20472f40c311d5
Brad Wilson <brad@lucky-dip.net>
2009-08-25 09:31:26 +1000

    fixed task list reload on ajax and save filter shadowbox

--- app/helpers/task_filter_helper.rb
+++ app/helpers/task_filter_helper.rb
@@ -75,7 +75,7 @@ module TaskFilterHelper
     form_remote_tag(:url => "/task_filters/update_current_filter", 
                     :html => { :method => "post", :id => "search_filter_form"},
                     :loading => "showProgress()",
-                    :complete => "hideProgress()",
+                    :complete => "hideProgress(); Shadowbox.setup();",
                     :update => "#content")
   end
 
