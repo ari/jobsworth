1d1e66400e127a1701c9724aafca5fda538b74e9
brad wilson <brad@delish.ish.com.au>
2009-08-26 18:01:07 +1000

    adding file perm changes

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+# just an error to ensure this doesn't get used while it's crashing the systme
+#1/0
 # Be sure to restart your web server when you modify this file.
 
 # Uncomment below to force Rails into production mode when
@@ -6,6 +8,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+
 
 
 # Bootstrap the Rails environment, frameworks, and default configuration
