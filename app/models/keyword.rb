c98c98ffe5a29f50fd5203dcf97425265192d80a
Brad Wilson <brad@lucky-dip.net>
2009-08-28 07:59:49 +1000

    fix bug in keyword saving

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -5,12 +5,14 @@ class Keyword < ActiveRecord::Base
   validates_presence_of :company
   validates_presence_of :task_filter
 
-  before_create :set_company_from_task_filter
+  before_validation :set_company_from_task_filter
 
   private
 
   def set_company_from_task_filter
-    self.company = task_filter.company || task_filter.user.company
+    if task_filter
+      self.company = task_filter.company || task_filter.user.company
+    end
   end
 
 end
