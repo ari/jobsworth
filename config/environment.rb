38e0d086b65870fd1d7da671d03a36534a1b6e49
Brad Wilson <brad@lucky-dip.net>
2009-09-11 16:16:39 +1000

    redefine csv for 1.9 in env.rb. all tests passing in 1.9 now

--- config/environment.rb
+++ config/environment.rb
@@ -72,6 +72,14 @@ Rails::Initializer.run do |config|
   if RUBY_VERSION < "1.9"
     # fastercsv has been moved in as default csv engine in 1.9
     config.gem 'fastercsv', :version => '1.5.0'
+  else
+    require "csv"
+    if !defined?(FasterCSV)
+      class Object
+        FasterCSV = CSV
+        alias_method :FasterCSV, :CSV
+      end
+    end
   end
 
 
