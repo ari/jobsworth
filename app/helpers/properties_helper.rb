e075f200e9ad2438b992c48df532994f3af20cfe
Lionel Bouton <lionel.bouton@jtek.fr>
2009-09-18 05:58:50 +0800

    Fix a Ruby warning for missing parentheses
    Signed-off-by: Ari Maniatis <ari@ish.com.au>

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,6 +1,6 @@
 module PropertiesHelper
   def add_value_link
-    link_to_function(_"Add") do |page|
+    link_to_function(_("Add")) do |page|
       pv = @property.property_values.build
       page.insert_html(:bottom, "property_values", :partial => "property_value", 
                        :locals => { :pv => pv })
