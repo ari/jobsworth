3a070d48d21884f47b877963b0ba60e09c8aca4f
Ari Maniatis <ari@ish.com.au>
2009-10-18 00:42:34 +1100

    Remove asset packager and replace with default Rails asset combining functionality which will be simpler and work better with Heroku.

--- public/javascripts/beast.js
+++ public/javascripts/beast.js
@@ -1,5 +1,4 @@
 // Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 
 var TopicForm = {
   editNewTitle: function(txtField) {
