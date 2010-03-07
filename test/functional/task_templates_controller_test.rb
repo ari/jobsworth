d4e18c791d23c8b06fbbb34c61f2da66410e431a
Anatoliy Lysenko <lysenko@a64.lh>
2010-03-07 16:17:43 +0200

    Add test cases for TaskTemplatesController#update. All passed.

--- test/functional/task_templates_controller_test.rb
+++ test/functional/task_templates_controller_test.rb
@@ -43,10 +43,21 @@ class TaskTemplatesControllerTest < ActionController::TestCase
     context 'when update task tamplate' do
       setup do
         @template = Template.first
-        @template.work_logs.clear
+        @template.users.clear
         @parameters={ :id=>@template.id,
-          :task=>{ :id=>@template.id, :name=>@template.name + '!!update!!', :description=> @template.description+'!!update!!'}
+          :task=>{
+            :id=>@template.id,
+            :name=>@template.name + '!!update!!',
+            :description=> @template.description+'!!update!!',
+            :properties=>{ },
+            :customer_attributes=>{ }
+          },
+          :users=> @user.company.user_ids,
+          :assigned=> [@user.id],
+          :notify=> @user.company.user_ids
         }
+        @user.company.properties.each{|p| @parameters[:task][:properties][p.id]=p.property_values.last.id }
+        @user.company.customers.each{|c| @parameters[:task][:customer_attributes][c.id]=c.id}
         post(:update, @parameters)
         @template.reload
       end
@@ -55,12 +66,20 @@ class TaskTemplatesControllerTest < ActionController::TestCase
         assert_equal @parameters[:task][:description], @template.description
       end
       should 'change custom property values' do
+        assert_not_equal 0, @user.company.properties.size
+        @user.company.properties.each do |p|
+          assert_not_nil @template.property_value(p)
+          assert_equal @parameters[:task][:properties][p.id], @template.property_value(p).id
+        end
       end
       should 'change todos' do
       end
       should 'change users' do
+        assert_same_elements @parameters[:users], @template.user_ids+@template.watcher_ids
+        assert_equal @parameters[:assigned], @template.user_ids
       end
       should 'change clients' do
+        assert_equal @parameters[:task][:customer_attributes].keys, @template.customer_ids
       end
       should 'not add any dependecies' do
         assert_equal 0, @template.dependencies.size
