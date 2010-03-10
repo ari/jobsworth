ef15a15ee73c08c05d2cbc2ccb62ea2516712a53
Anatoliy Lysenko <lysenko@a64.lh>
2010-03-10 18:09:14 +0200

    User should create task form template using templates list under "Task" menu. Revert "Add "Create task from this template" button to task template edit page. Unobtrusive onclick handler change form action to tasks/create. When user click "Create task from this template", request sended to TasksController#create, so  one task creation code work in both cases(from tasks/new and from template)."
    This reverts commit d8208de0c8e817ac8cd0b5b25e3fe93f30d49aa4.

--- app/helpers/task_templates_helper.rb
+++ app/helpers/task_templates_helper.rb
@@ -1,7 +1,2 @@
 module TaskTemplatesHelper
- def create_task_from_template_tag(object)
-    unless object.new_record?
-      submit_tag(_("Create task from this template"), :id=>'create_task_from_template', :class => 'nolabel', :style=>'float:none;')
-    end
-  end
 end
