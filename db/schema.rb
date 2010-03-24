9ddf63ea19c91d53f8f001de1213b79faa8b2ccd
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-03-24 12:50:41 +0200

    Add rake task to show number of broken tasks, run it by rake clockingit:show_number_of_broken_tasks

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100322160212) do
+ActiveRecord::Schema.define(:version => 20100322071846) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id",       :default => 0,  :null => false
