50daad0c59211d34ca4a5bbe29e6698c5f588434
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-03-24 11:53:22 +0200

    Change delete_in_progress migration to change task status even if task invalid.

--- db/migrate/20100322160212_delete_in_progress.rb
+++ db/migrate/20100322160212_delete_in_progress.rb
@@ -1,9 +1,10 @@
 class DeleteInProgress < ActiveRecord::Migration
   def self.up
-     Task.all.each do |t| 
+     Task.all.each do |t|
       if t.status>0 then
         t.status-= 1
-        t.save!
+        #even if task not valid, we must change status
+        t.save(false)
       end
     end
     Status.find_all_by_name("in progress").each{|s| s.destroy}
