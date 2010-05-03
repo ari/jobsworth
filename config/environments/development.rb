f37ec032764135cb8b2e264d81c0fcb96826b9ba
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-05-03 14:33:13 +0300

    Do not show bullet alerts in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ config.gem 'bullet'
 
 config.after_initialize do 
   Bullet.enable = true
-  Bullet.alert = true
+  Bullet.alert = false
   Bullet.bullet_logger= true
   Bullet.console = true
   Bullet.rails_logger = true
