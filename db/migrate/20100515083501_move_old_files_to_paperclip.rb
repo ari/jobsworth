c294beef3381a46d9cfa19581e64d7fe6bdfbb34
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-05-16 13:39:08 +0300

    Add more clear error messaged to move_old_files_to_paperclip migration.

--- db/migrate/20100515083501_move_old_files_to_paperclip.rb
+++ db/migrate/20100515083501_move_old_files_to_paperclip.rb
@@ -5,8 +5,16 @@ class MoveOldFilesToPaperclip < ActiveRecord::Migration
       store_name =  "#{file.id}#{"_" + file.task_id.to_s if file.task_id.to_i > 0}_#{filename}"
       full_name= File.join("#{RAILS_ROOT}", 'store', file.company_id.to_s, store_name)
       if File.exist?(full_name)
-        file.file= File.new(full_name)
-        file.save!
+        begin
+          file.file= File.new(full_name)
+          file.save!
+        rescue Exception => e
+          puts "Call to parerclip raise an exception"
+          puts "File: #{full_name}"
+          puts "Paperclip.options hash:"
+          p Paperclip.options
+          raise e
+        end
       end
     end
   end
