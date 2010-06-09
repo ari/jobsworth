d7f61274f54f0f74c3b7537c6814043bfcd0383d
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-06-09 10:39:39 +0300

    Fix for our custom atocomplete. It should be changed by jquery-ui autocomplete.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -898,9 +898,6 @@ function autocomplete(input_field, output_list, path, after_callback) {
                  }
               });
             });
-            jQuery(input_field).blur(function(){
-                jQuery(output_list).hide();
-            });
 }
 
 function init_task_form()
