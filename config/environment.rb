8f258ab966351b42aac3e51873667e0c038d37d4
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-07-21 14:10:33 +0300

    First step to upgrade to Rails 2.3.8. New ruby version not include current directory inrequire path($: variable) for security reasons. I am break this rule now.

--- config/environment.rb
+++ config/environment.rb
@@ -7,13 +7,14 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-
+$: << RAILS_ROOT
+$: << '.'
 require File.join(File.dirname(__FILE__), '../lib/localization.rb')
 Localization.load
 
