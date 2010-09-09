5ec60620dd1efbf5da362b2d5f67d2ad0e82bdd0
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-09-09 14:53:18 +0300

    Fix filter on list/calendar pages. I use hand made polimorh function taskViewReload, it will reload list when we on task list or calendar when we on calendar page. On other pages(like reports) we reload whole page.

--- public/javascripts/filter.js
+++ public/javascripts/filter.js
@@ -3,14 +3,7 @@ Submits the search filter form. If we are looking at the task list,
 does that via ajax. Otherwise does a normal html post
 */
 function submitSearchFilterForm() {
-  var form = jQuery("#search_filter_form")[0];
-  var redirect = jQuery(form.redirect_action).val();
-  if (redirect.indexOf("/tasks/list") >= 0) {
-      form.trigger('submit');
-  }
-  else {
-    form.submit();
-  }
+    jQuery("#search_filter_form").trigger('submit');
 }
 
 /*
@@ -104,7 +97,7 @@ function loadFilter(data, url){
   jQuery('#search_filter_keys').effect("highlight", {color: '#FF9900'}, 3000);
   jQuery.ajax({
             beforeSend: function(){ showProgress(); },
-            complete: function(request){ tasklistReload(); hideProgress(); } ,
+            complete: function(request){ tasksViewReload(); hideProgress(); } ,
             data: data,
             success:  function(request){jQuery('#search_filter_keys').html(request);},
             type:'post',
@@ -113,9 +106,19 @@ function loadFilter(data, url){
         return false;
 }
 
+function tasksViewReload()
+{
+    if(/tasks\/list$/.test(document.location.href)) {
+        return tasklistReload();
+    }
+    if(/tasks\/calendar$/.test(document.location.href)) {
+        return calendarReload();
+    }
+}
+
 jQuery(document).ready(function() {
-  //only if we on task list page
-  if( /tasks\/list$/.test(document.location.href) ){
+  //only if we on tasks list or calendar or gantt page
+    if( /tasks\/(list|calendar|gantt)$/.test(document.location.href) ){
       jQuery("#search_filter_form").submit(function(event){
         return loadFilter(jQuery.param(jQuery(this).serializeArray()), "/task_filters/update_current_filter");
       });
--- public/javascripts/tasklist.js
+++ public/javascripts/tasklist.js
@@ -9,8 +9,8 @@ var columnModel;
   Sends an ajax request to save the given user preference to the db
 */
 function saveUserPreference(name, value) {
-	var params = { "name": name, "value": value };
-	jQuery.post("/users/set_preference",  params);
+        var params = { "name": name, "value": value };
+        jQuery.post("/users/set_preference",  params);
 }
 function getUserPreference(name) {
   var url = "/users/get_preference?name=" + name;
@@ -18,34 +18,34 @@ function getUserPreference(name) {
 }
 
 function tasklistReload() {
-	jQuery("#task_list").trigger("reloadGrid");
+        jQuery("#task_list").trigger("reloadGrid");
 }
 
 
 function selectRow(rowid) {
-	jQuery('#task_list').setCell(rowid, 'read', true);
-	jQuery('#task_list>tbody>tr#' + rowid).removeClass('unread');
-	loadTask(rowid);
+        jQuery('#task_list').setCell(rowid, 'read', true);
+        jQuery('#task_list>tbody>tr#' + rowid).removeClass('unread');
+        loadTask(rowid);
 }
 
 function setRowReadStatus(rowid, rowdata) {
-	if (rowdata.read == 'f') {
-		jQuery('#task_list>tbody>tr#' + rowid).addClass('unread');
-	}
+        if (rowdata.read == 'f') {
+                jQuery('#task_list>tbody>tr#' + rowid).addClass('unread');
+        }
 }
 
 function taskListConfigSerialise() {
-	var model = jQuery("#task_list").jqGrid('getGridParam', 'colModel');
+        var model = jQuery("#task_list").jqGrid('getGridParam', 'colModel');
 
-	jQuery.ajax({
-		type: "POST",
-		url: '/users/set_tasklistcols',
-		data: { model : JSON.stringify(model)},
-		dataType: 'json',
-		success: function(msg) {
-			alert( "Data Saved: " + msg );
-		}
-	});
+        jQuery.ajax({
+                type: "POST",
+                url: '/users/set_tasklistcols',
+                data: { model : JSON.stringify(model)},
+                dataType: 'json',
+                success: function(msg) {
+                        alert( "Data Saved: " + msg );
+                }
+        });
 }
 
 function change_group() {
@@ -81,109 +81,109 @@ jQuery(document).ready(function() {
   });
 });
 
-function initTaskList() {  
+function initTaskList() {
   jQuery('#task_list').jqGrid({
-  	url : '/tasks/list?format=json',
-  	datatype: 'json',
-  	jsonReader: {
-  		root: "tasks.rows",
-  		repeatitems:false
-  	},
-  	colModel : columnModel.colModel,
-  	loadonce: false,
-  	sortable : function(permutation) { taskListConfigSerialise(); }, // re-order columns
-  	sortname: columnModel.currentSort.column,
-  	sortorder: columnModel.currentSort.order,
-  	
-  	caption: "Tasks",
-  	viewrecords: true,
-  	multiselect: false,
-  	
-  	afterInsertRow : function(rowid, rowdata, rowelem) { setRowReadStatus(rowid, rowdata); },
-  	onSelectRow: function(rowid, status) { selectRow(rowid); },
-  	resizeStop: function(newwidth, index) { taskListConfigSerialise(); },
-  	shrinkToFit: true,
-  	
-  	pager: '#task_pager',
-  	emptyrecords: 'No tasks found.',
-  	pgbuttons:false,
-  	pginput:false,
-  	rowNum:200,
-  	recordtext: '{2} tasks found.',
-  	
-  	footerrow: true,
-  	userDataOnFooter: true,
-  	userdata: "userdata",
-  	
-  	height: 300,
-  	width: 500,
-        
-  	grouping: jQuery("#chngroup").val() != "clear",
-  	groupingView: {
+        url : '/tasks/list?format=json',
+        datatype: 'json',
+        jsonReader: {
+                root: "tasks.rows",
+                repeatitems:false
+        },
+        colModel : columnModel.colModel,
+        loadonce: false,
+        sortable : function(permutation) { taskListConfigSerialise(); }, // re-order columns
+        sortname: columnModel.currentSort.column,
+        sortorder: columnModel.currentSort.order,
+
+        caption: "Tasks",
+        viewrecords: true,
+        multiselect: false,
+
+        afterInsertRow : function(rowid, rowdata, rowelem) { setRowReadStatus(rowid, rowdata); },
+        onSelectRow: function(rowid, status) { selectRow(rowid); },
+        resizeStop: function(newwidth, index) { taskListConfigSerialise(); },
+        shrinkToFit: true,
+
+        pager: '#task_pager',
+        emptyrecords: 'No tasks found.',
+        pgbuttons:false,
+        pginput:false,
+        rowNum:200,
+        recordtext: '{2} tasks found.',
+
+        footerrow: true,
+        userDataOnFooter: true,
+        userdata: "userdata",
+
+        height: 300,
+        width: 500,
+
+        grouping: jQuery("#chngroup").val() != "clear",
+        groupingView: {
       groupField: [jQuery("#chngroup").val()]
-  	}
+        }
   });
-  	
+
   jQuery('#task_list').navGrid('#task_pager', {refresh:true, search:false, add:false, edit:false, view:false, del:false},
-  	{}, // use default settings for edit
-  	{}, // use default settings for add
-  	{}, // use default settings for delete
-  	{}, // use default settings for search
-  	{} // use default settings for view
+        {}, // use default settings for edit
+        {}, // use default settings for add
+        {}, // use default settings for delete
+        {}, // use default settings for search
+        {} // use default settings for view
   );
-  	
+
   jQuery("#task_list").jqGrid('sortableRows', {
     update: function(event, ui) {
     if (jQuery("#chngroup").val() != "clear") {
-  		var id = ui.item.index();
-  		for (i=id;i>=0;i--) {
-  			if (jQuery("tbody.ui-sortable > tr.ui-widget-content").eq(i-1).attr("id").match(/task_listghead/) != null) {	
-      		var group_id = jQuery("tbody.ui-sortable > tr.ui-widget-content").eq(i-1).attr("id");
-    			var group_text = jQuery("#" + group_id + " > td").text();
-      		var group_icon;
-      		if (group_text == "") {
-      			group_icon = jQuery("#" + group_id + " > td > img").attr("src");
-      		}
-    			break;
-  			}
-  		}
-  		var group = jQuery("#chngroup").val();
-  		jQuery.post("/tasks/set_group/"+ ui.item.attr("id") +"?group=" +  group + "&value=" + group_text+ "&icon=" + group_icon);
-      	if (group_text != "") {
-      		jQuery('.ui-sortable > tr#'+ ui.item.attr("id") +' > td[aria-describedby=\"task_list_'+ group + '\"]').text(group_text);
-      		jQuery('.ui-sortable > tr#'+ ui.item.attr("id") +' > td[aria-describedby=\"task_list_'+ group + '\"]').attr('title', group_text);
-      	} else if(group_icon != undefined) {
-      		var image = jQuery("#" + group_id + " > td").html();
-      		jQuery('.ui-sortable > tr#'+ ui.item.attr("id") +' > td[aria-describedby=\"task_list_'+ group + '\"]').html(image);
-      		jQuery('.ui-sortable > tr#'+ ui.item.attr("id") +' > td[aria-describedby=\"task_list_'+ group + '\"] > span.ui-icon').remove();
-      	}
-    	}
+                var id = ui.item.index();
+                for (i=id;i>=0;i--) {
+                        if (jQuery("tbody.ui-sortable > tr.ui-widget-content").eq(i-1).attr("id").match(/task_listghead/) != null) {
+                var group_id = jQuery("tbody.ui-sortable > tr.ui-widget-content").eq(i-1).attr("id");
+                        var group_text = jQuery("#" + group_id + " > td").text();
+                var group_icon;
+                if (group_text == "") {
+                        group_icon = jQuery("#" + group_id + " > td > img").attr("src");
+                }
+                        break;
+                        }
+                }
+                var group = jQuery("#chngroup").val();
+                jQuery.post("/tasks/set_group/"+ ui.item.attr("id") +"?group=" +  group + "&value=" + group_text+ "&icon=" + group_icon);
+        if (group_text != "") {
+                jQuery('.ui-sortable > tr#'+ ui.item.attr("id") +' > td[aria-describedby=\"task_list_'+ group + '\"]').text(group_text);
+                jQuery('.ui-sortable > tr#'+ ui.item.attr("id") +' > td[aria-describedby=\"task_list_'+ group + '\"]').attr('title', group_text);
+        } else if(group_icon != undefined) {
+                var image = jQuery("#" + group_id + " > td").html();
+                jQuery('.ui-sortable > tr#'+ ui.item.attr("id") +' > td[aria-describedby=\"task_list_'+ group + '\"]').html(image);
+                jQuery('.ui-sortable > tr#'+ ui.item.attr("id") +' > td[aria-describedby=\"task_list_'+ group + '\"] > span.ui-icon').remove();
+        }
+        }
     }
   });
-  
+
   jQuery("#task_list").jqGrid('gridResize', {
-  	stop: function(event, ui) {
-  		resizeGrid(); // force width
+        stop: function(event, ui) {
+                resizeGrid(); // force width
     },
     minHeight: 150,
     maxHeight: 1000
   });
-  
+
   jQuery("#task_list").jqGrid('navButtonAdd','#task_pager', {
-  	caption: "Columns",
-  	title: "Show/hide columns",
-  	onClickButton : function () {
-  		jQuery("#task_list").jqGrid('columnChooser');
-  		taskListConfigSerialise();
-  	}
+        caption: "Columns",
+        title: "Show/hide columns",
+        onClickButton : function () {
+                jQuery("#task_list").jqGrid('columnChooser');
+                taskListConfigSerialise();
+        }
   });
-  
+
   jQuery("#task_list").jqGrid('navButtonAdd','#task_pager', {
-  	caption: "Export",
-  	title: "Export data to CSV",
-  	onClickButton : function () {
+        caption: "Export",
+        title: "Export data to CSV",
+        onClickButton : function () {
       window.location.href="/tasks/get_csv";
-  	}
+        }
   });
 
   appendPartial("/task_filters/new", '#savefilter', false);
@@ -193,15 +193,15 @@ function initTaskList() {
     onClickButton : function () {
       var dialog = jQuery("#savefilter").dialog({
         width: 400,
-	    autoOpen: false,
-	    title: 'Save Filter',
+            autoOpen: false,
+            title: 'Save Filter',
         draggable: true
-	  });
-	  dialog.dialog('open');
-	  return false;
+          });
+          dialog.dialog('open');
+          return false;
     }
   });
-  
+
   jQuery("#task_list").jqGrid('navButtonAdd','#task_pager', {
       caption: jQuery("#groupby").html(),
       buttonicon: "none",
@@ -269,51 +269,56 @@ jQuery(document).ready(function() {
         // FIXME: needs ajax callback to update task date
       }
 
-	});
+        });
 });
 
 function dueTaskValue(cellvalue) {
-	if (cellvalue == "") {
-	    return "";
-	}
-	var one_day=1000*60*60*24;
-	var days = Math.round( (new Date(cellvalue * 1000) - new Date().getTime()) /one_day);
-	if (days == 0) {
+        if (cellvalue == "") {
+            return "";
+        }
+        var one_day=1000*60*60*24;
+        var days = Math.round( (new Date(cellvalue * 1000) - new Date().getTime()) /one_day);
+        if (days == 0) {
     return "<span class='due_today'>today</span>";
-	}
-	if (days == 1) {
+        }
+        if (days == 1) {
     return "<span class='due_future'>tomorrow</span>";
-	}
-	if (days == -1) {
+        }
+        if (days == -1) {
     return "<span class='due_past'>" + "yesterday</span>";
-	}
-	if (days > 548) {
+        }
+        if (days > 548) {
     return "<span class='due_future'>" + Math.round(days/365) + " years</span>";
-	}
-	if (days < -548) {
+        }
+        if (days < -548) {
     return "<span class='due_past'>" + Math.round(-days/365) + " years ago</span>";
-	}
-	if (days > 50) {
+        }
+        if (days > 50) {
     return "<span class='due_future'>" + Math.round(days/30.4) + " months</span>"; // average number of days in a month
-	}
-	if (days < -50) {
+        }
+        if (days < -50) {
     return "<span class='due_past'>" + Math.round(-days/30.4) + " months ago</span>";
-	}
-	if (days > 14) {
+        }
+        if (days > 14) {
     return "<span class='due_future'>" + Math.round(days/7) + " weeks</span>";
-	}
-	if (days < -14) {
+        }
+        if (days < -14) {
     return "<span class='due_past'>" + Math.round(-days/7) + " weeks ago</span>";
-	}
-	if (days > 0) {
+        }
+        if (days > 0) {
     return "<span class='due_future'>" + days + " days</span>";
-	}
-	return "<span class='due_past'>" + -days + " days ago</span>";
+        }
+        return "<span class='due_past'>" + -days + " days ago</span>";
 }
 
 function timeTaskValue(cellvalue) {
-	if (cellvalue == 0) {
-	  return "";
-	}
-	return Math.round(cellvalue/6)/10 + "hr";
+        if (cellvalue == 0) {
+          return "";
+        }
+        return Math.round(cellvalue/6)/10 + "hr";
+}
+
+function calendarReload()
+{
+    jQuery('#calendar').fullCalendar('refetchEvents');
 }
