0d8f8029386a0b8131a4582c9be75aa63b645b59
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-09-13 17:34:41 +0300

    Remove code duplication in Task.

--- app/models/task.rb
+++ app/models/task.rb
@@ -593,7 +593,7 @@ class Task < ActiveRecord::Base
   end
 
   def to_csv
-    [project.customer.name, project.name, task_num, name, tags.collect(&:name).join(','), owners_to_display, milestone.nil? ? nil : milestone.name, due_at.nil? ? milestone.nil? ? nil : milestone.due_at : due_at, created_at, completed_at, worked_minutes, duration, status_type ] +
+    [project.customer.name, project.name, task_num, name, tags.collect(&:name).join(','), owners_to_display, milestone.nil? ? nil : milestone.name, self.due_date, created_at, completed_at, worked_minutes, duration, status_type ] +
       company.properties.collect { |property| property_value(property).to_s }
   end
 
