e8a325c1ab24bb895ba8c277c4acc25ea3708585
Nugi Nugraha <nugi@prawirasoft.com>
2010-09-27 06:11:09 +0700

    fix integration tests : undefined method 'visit'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ require File.expand_path(File.dirname(__FILE__) + "/blueprints")
 
 require "webrat"
 Webrat.configure do |config|
-  config.mode = :rails
+  config.mode = :rake
   config.open_error_files = false
 end
 
