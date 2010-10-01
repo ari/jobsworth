5372af0ed4a36f6338650688d808b1273dad1861
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-10-01 13:28:29 +0300

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# This file is auto-generated from the current state of the database. Instead of editing this file,
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
@@ -611,7 +611,7 @@ ActiveRecord::Schema.define(:version => 20100920121117) do
   create_table "task_users", :force => true do |t|
     t.integer  "user_id"
     t.integer  "task_id"
-    t.string   "type"
+    t.string   "type", :default => "TaskOwner"
     t.boolean  "unread"
     t.datetime "created_at"
     t.datetime "updated_at"
