29f989610fd010ceb64b06b4b6c0449b47d8fc39
Elena Dzisyuk <dzisyuk.elen@gmail.com>
2010-10-06 11:11:42 +0300

    Fixed tag output in the tooltip after the call task#escape_twice. In any case, in the tooltip we can not mousever on the link. Using tags with links it makes no sense.

--- app/models/task.rb
+++ app/models/task.rb
@@ -500,7 +500,7 @@ class Task < ActiveRecord::Base
       res = "<table id=\"task_tooltip\" cellpadding=0 cellspacing=0>"
       res << "<tr><th>#{_('Summary')}</td><td>#{escape_twice(self.name)}</tr>"
       res << "<tr><th>#{_('Project')}</td><td>#{escape_twice(self.project.full_name)}</td></tr>"
-      res << "<tr><th>#{_('Tags')}</td><td>#{escape_twice(self.full_tags)}</td></tr>" unless self.full_tags.blank?
+      res << "<tr><th>#{_('Tags')}</td><td>#{escape_twice(self.full_tags_without_links)}</td></tr>" unless self.full_tags.blank?
       res << "<tr><th>#{_('Assigned To')}</td><td>#{escape_twice(owners)}</td></tr>"
       res << "<tr><th>#{_('Requested By')}</td><td>#{escape_twice(self.requested_by)}</td></tr>" unless self.requested_by.blank?
       res << "<tr><th>#{_('Resolution')}</td><td>#{_(self.status_type)}</td></tr>"
