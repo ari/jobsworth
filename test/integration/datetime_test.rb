9f5ae56f2e9c792498bd598df3a1b986b930ca64
Elena Dzisyuk <dzisyuk.elen@gmail.com>
2010-10-27 13:41:39 +0300

    Add test for tasks/edit log_time аfter editing work_log.

--- test/integration/datetime_test.rb
+++ test/integration/datetime_test.rb
@@ -8,37 +8,48 @@ class DatetimeTest < ActionController::IntegrationTest
           @task =@project.tasks.first
           visit ('/tasks/edit/'+@task.task_num.to_s)
         end
-        should "see local current time in field Start" do     
+        should "see local current time in field Start" do
           assert_equal @user.tz.utc_to_local(Time.now.utc).strftime(@user.date_format + ' ' + @user.time_format), find_by_id('work_log_started_at').value
         end
-        context "in task_history -> log_time, when add comment with 'Time spent'" do
+        context "and add comment with 'Time spent'" do
           setup do
             fill_in "comment", :with => "my new comment"
             fill_in "work_log_duration", :with => "5m"
           end
-          should "see the same start time, which was in the field 'Start', when not change 'Start' time" do
-            start_datetime= find_by_id('work_log_started_at').value.split(' ').second
-            click_button "Save"
-            start_log_time= find(:css, '.log_time').text.split('-').first.gsub(/\s/,'')
-            assert_equal start_datetime, start_log_time
+          context "in task_history -> log_time" do
+            should "see the same start time, as was in the field 'Start', when not change 'Start' time" do
+              start_datetime= find_by_id('work_log_started_at').value.split(' ').second
+              click_button "Save"
+              start_log_time= find(:css, '.log_time').text.split('-').first.gsub(/\s/,'')
+              assert_equal start_datetime, start_log_time
+            end
+            should "see the same start time, which was in the field 'Start', when change 'Start' time" do
+              start_datetime= fill_in('work_log_started_at', :with =>"26/10/2010 11:50").split(' ').second
+              click_button "Save"
+              start_log_time= find(:css, '.log_time').text.split('-').first.gsub(/\s/,'')
+              assert_equal start_datetime, start_log_time
+            end
           end
-          should "see the same start time, which was in the field 'Start', when change 'Start' time" do
-            start_datetime= fill_in('work_log_started_at', :with =>"26/10/2010 11:50").split(' ').second
-            click_button "Save"
-            start_log_time= find(:css, '.log_time').text.split('-').first.gsub(/\s/,'')
-            assert_equal start_datetime, start_log_time
-          end
-          context "click to /work_logs/edit link" do
-            should "see in work_logs/edit Start field the same time, as in tasks/edit Start field" do
-              start_datetime_task_edit= find_by_id('work_log_started_at').value
+          context "and click to /work_logs/edit link" do
+            setup do
+              @start_datetime= find_by_id('work_log_started_at').value
               click_button "Save"
               click_link('5m')
+            end
+            should "see in work_logs/edit 'Start' field the same time, as in tasks/edit 'Start' field" do
               start_datetime_log_edit= find_by_id('work_log_started_at').value
-              assert_equal start_datetime_task_edit, start_datetime_log_edit
+              assert_equal @start_datetime, start_datetime_log_edit
+            end
+            should "see in work_logs/edit 'Start' field the same time, as in tasks/edit log_time, when click save and follow to task edit" do
+              start_datetime_log_edit= find_by_id('work_log_started_at').value.split(' ').second
+              fill_in('work_log_body', :with => "new text")
+              click_button "Save"
+              start_log_time= find(:css, '.log_time').text.split('-').first.gsub(/\s/,'')
+              assert_equal start_datetime_log_edit, start_log_time
             end
           end
         end
-      end
+    end
   end
   context "A logged in user" do 
     setup do 
