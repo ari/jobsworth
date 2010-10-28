5b0c63517da7890f3d4fa02a842ea7236cbb828e
Nugi Nugraha <nugi@prawirasoft.com>
2010-10-29 05:38:19 +0700

    fix invalid link_to (ajax)

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -46,7 +46,7 @@ module TodosHelper
   def add_new_todo
     link_to_function(_("New To-do Item")) do |page|
       todo = Todo.new(:creator_id => current_user.id)
-      page << "jQuery('#todos-clone').prepend('#{escape_javascript render_to_string(:partial => "/todos/new_todo", :locals => { :todo => todo})}')"
+      page << "jQuery('#todos-clone').append('#{escape_javascript render_to_string(:partial => "/todos/new_todo", :locals => { :todo => todo})}')"
       page << "addNewTodoKeyListenerForUncreatedTask(this, 'new');new_task_form();"
     end
   end
