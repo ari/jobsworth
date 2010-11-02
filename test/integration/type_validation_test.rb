1b9790bd4b51e586eee8ee944b0a3969b6a86662
Elena Dzisyuk <dzisyuk.elen@gmail.com>
2010-11-02 15:14:33 +0200

    Created a test for validation the type, when the Type property is mandatory and the task's Type is not selected.

---
+++ test/integration/type_validation_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class TypeValidationTest < ActionController::IntegrationTest
+  context "A logged in user with existings projects" do
+    setup do
+      @user = login
+      @project = project_with_some_tasks(@user)
+      @milestone =  Milestone.make(:project => @project, :user => @user,
+                                   :company => @project.company)
+    end
+    context "when the Type property is mandatory and task's Type is not selected" do
+        setup do
+          type= @user.company.properties.where("name=?","Type").first
+          type.mandatory= true
+          type.save!
+        end
+        context "creating a new task" do
+          setup do
+            visit "/"
+            click_link "New Task"
+            fill_in "task_name", :with => "a brand new task"
+            fill_in "task_description", :with => "a new desc"
+            select @project.name, :from => "Project"
+            select @milestone.name, :from => "Milestone"
+          end
+          should "be validation message: Type is required, and task sould not be created" do
+            @task_count= Task.count
+            select "", :from => "Type"
+            click_button "Create"
+            task_count2= Task.count
+            assert_equal @task_count, task_count2
+            assert page.has_content?("Type is required")
+          end
+        end
+      end
+  end
+end
