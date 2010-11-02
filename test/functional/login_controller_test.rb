b185d75862b674c7825042ba4912f5092e160fd5
Nugi Nugraha <nugi@prawirasoft.com>
2010-11-02 10:53:52 +0700

    remove unused test

--- test/functional/login_controller_test.rb
+++ test/functional/login_controller_test.rb
@@ -12,23 +12,6 @@ class LoginControllerTest < ActionController::TestCase
 #     get :list
 #     response.should.redirect :controller => 'login', :action => 'login'
 #   end
-
-  test "should be able to signup" do
-    user_count = User.count
-    company_count = Company.count
-    customer_count = Customer.count
-    email_address_count = EmailAddress.count
-    
-    post :take_signup, { :username => 'newuser', :password => 'newpassword', :password_again => 'newpassword', :name => "New User", :email => "new@clockingit.com", :company => 'New Company',
-      :subdomain => 'newsubdomain', :user => {:time_zone => 'Europe/Oslo' } }
-
-    assert_equal User.count, user_count + 1
-    assert_equal Company.count, company_count + 1
-    assert_equal Customer.count, customer_count + 1
-    assert_equal EmailAddress.count, email_address_count  + 1
-
-    assert_response 302
-  end 
   
   test "should not login without username and password" do
     post :validate, :user => { 'username' => '', 'password' => '' }
