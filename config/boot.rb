c7ab2ec9507dbd92fa37489244cdbd2fe6b44040
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-11-03 12:53:25 +0200

    We should inform bundler about Gemfile on boot. Without this, executing rails from not RAILS_ROOT dir(like /dir/dir/squish/script/rails) will raise GemfileNotFound error.

--- config/boot.rb
+++ config/boot.rb
@@ -2,5 +2,6 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 if File.exist?(File.expand_path('../../Gemfile', __FILE__))
   require 'bundler'
+  ENV['BUNDLE_GEMFILE']=File.expand_path('../../Gemfile', __FILE__)
   Bundler.setup
 end
