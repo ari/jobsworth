dd20739b233cc9b7088ac5cf60887dba4be6a7ca
Elena Dzisyuk <dzisyuk.elen@gmail.com>
2010-11-12 12:48:12 +0200

    Fix broken todos.

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -7,9 +7,9 @@ module TodosHelper
   end
 
   def todo_open_close_check_box(todo)
-    title = _('Open <b>%s</b>', todo.name).html_safe
+    title = _('Open <b>%s</b>', h(todo.name))
     if todo.done?
-      title = _("Close <b>%s</b", todo.name).html_safe
+      title = _("Close <b>%s</b>", h(todo.name))
     end
 
     url = "/todos/toggle_done/#{ todo.id }?task_id=#{ @task.id }"
@@ -24,9 +24,9 @@ module TodosHelper
   end
 
   def new_todo_open_close_check_box(todo)
-    title = _('Open <b>%s</b>', todo.name).html_safe
+    title = _('Open <b>%s</b>', h(todo.name))
     if todo.done?
-      title = _("Close <b>%s</b", todo.name).html_safe
+      title = _("Close <b>%s</b>", h(todo.name))
     end
 
     check_box("todo", "done", { :title => title,
