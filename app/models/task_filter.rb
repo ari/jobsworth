0e84b3d7514a3d4e5bdc1a7ca9df4d9a8f3f250a
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-11-23 20:00:51 +0200

    Use one transaction inside TaskFilter#store_for and TaskFilter#update_filter for performance reason. Test added, it show small speedup, but in production it can fix bottleneck in TaskFiltersController#update_current_filter.

--- app/models/task_filter.rb
+++ app/models/task_filter.rb
@@ -138,22 +138,26 @@ class TaskFilter < ActiveRecord::Base
   end
 
   def store_for(user)
-    if (TaskFilter.recent_for(user).count >= 10)
-      TaskFilter.recent_for(user).last.destroy
+    ActiveRecord::Base.transaction do
+      if (TaskFilter.recent_for(user).count >= 10)
+        TaskFilter.recent_for(user).last.destroy
+      end
+      filter=TaskFilter.new(:recent_for_user_id=>user.id, :user=>user, :company=>self.company)
+      filter.name= generate_name
+      filter.name= self.name if filter.name.blank?
+      filter.copy_from(self)
+      filter.save!
     end
-    filter=TaskFilter.new(:recent_for_user_id=>user.id, :user=>user, :company=>self.company)
-    filter.name= generate_name
-    filter.name= self.name if filter.name.blank?
-    filter.copy_from(self)
-    filter.save!
   end
 
   def update_filter(params)
-    self.keywords.clear
-    self.qualifiers.clear
-    self.unread_only = false
-    self.attributes = params
-    self.save!
+    ActiveRecord::Base.transaction do
+      self.keywords.clear
+      self.qualifiers.clear
+      self.unread_only = false
+      self.attributes = params
+      self.save!
+    end
   end
 
 private
