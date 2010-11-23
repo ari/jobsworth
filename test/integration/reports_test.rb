c364ceb09a10a5dbe7fe61a4ead34380000f388c
Elena Dzisyuk <dzisyuk.elen@gmail.com>
2010-11-23 15:28:32 +0200

    Created test for report's Time Ranges 'Last Year' and 'Last Month'. Bug with 'Last Year' and 'Last Month' time ranges was fixed in the commit 1bc6038.

---
+++ test/integration/reports_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class ReportsTest < ActionDispatch::IntegrationTest
+  context "a logged in user" do
+    setup do
+      @user= login
+      @user.option_tracktime= true
+      @user.admin= true
+      @user.save!
+    end
+    context "with some existing tasks" do
+      setup do
+        @project= project_with_some_tasks(@user)
+        visit "/"
+        @task= @project.tasks.first
+        visit ('/tasks/edit/'+@task.task_num.to_s)
+        fill_in "comment", :with => "my new comment"
+        fill_in "work_log_duration", :with => "5m"
+      end
+      context "when select Time Range- Last Year in report" do
+        should "be in the report's table link to the task, on which work" do
+          fill_in('work_log_started_at', :with => (Time.now - 1.year - 1.month).strftime(@user.date_format+' '+@user.time_format))
+          click_button('Save')
+          click_link('Reports')
+          select('Last Year', :from => 'Time Range')
+          click_button('Run Report')
+          link= find(:css,'.row_heading a')
+          assert_equal @task.name, link.text
+        end
+      end
+      context "select Time Range- Last Month in report" do
+        should "be in the report's table link to the task, on which work" do
+          fill_in('work_log_started_at', :with => (Time.now - 1.month).strftime(@user.date_format+' '+@user.time_format))
+          fill_in('work_log_started_at', :with => "23/10/2010 11:00")
+          click_button('Save')
+          click_link('Reports')
+          select('Last Month', :from => 'Time Range')
+          click_button('Run Report')
+          link= find(:css, '.row_heading a')
+          assert_equal @task.name, link.text
+        end
+      end
+    end
+  end
+end
