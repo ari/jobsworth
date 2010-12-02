be04a4fa6723eb4d464d5ba8ffedb5b5407ae142
Nugi Nugraha <nugi@prawirasoft.com>
2010-12-02 11:48:48 +0700

    set the expire time for session to 3 hours

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, :key => '_clockingit_session'
+Rails.application.config.session_store :cookie_store, :key => '_clockingit_session', :expire_after => 3.hours
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
