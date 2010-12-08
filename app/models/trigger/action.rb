c002e1fe06d49bd60c0e745283744e2e30965d3f
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-12-08 16:26:36 +0200

    Show actions names on triggers/index page.

--- app/models/trigger/action.rb
+++ app/models/trigger/action.rb
@@ -3,6 +3,10 @@ class Trigger::Action < ActiveRecord::Base
   attr_protected :type
   belongs_to :trigger
 
+  def name
+    self.class.name.demodulize.underscore.humanize
+  end
+
   def execute(task)
     raise "Subclass should reimplement execute action"
   end
