678635f90911147d38ae8a96667b9180bd899b05
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2010-12-13 13:01:40 +0200

    Remove dumb actions from TasksTemplatesController. No one call them, so we do not need them.

--- app/controllers/task_templates_controller.rb
+++ app/controllers/task_templates_controller.rb
@@ -1,34 +1,5 @@
 # encoding: UTF-8
 class TaskTemplatesController < TasksController
-#this actions defined in TasksController but unused in TasksTemplatesController
-#they never called, but if some code call one of them, we need to know
-#TODO: all this actions must be changed in production
-  CUSTOM_ERROR_MESSAGE="tasks_tempaltes don't have this action, only tasks have "
-  def  auto_complete_for_resource_name
-    raise Exception, CUSTOM_ERROR_MESSAGE
-  end
-  def resource
-    raise Exception, CUSTOM_ERROR_MESSAGE
-  end
-  def dependency
-    raise Exception, CUSTOM_ERROR_MESSAGE
-  end
-  def ajax_restore
-    raise Exception, CUSTOM_ERROR_MESSAGE
-  end
-  def ajax_hide
-    raise Exception, CUSTOM_ERROR_MESSAGE
-  end
-  def updatelog
-    raise Exception, CUSTOM_ERROR_MESSAGE
-  end
-  def update_sheet_info
-     raise Exception, CUSTOM_ERROR_MESSAGE
-  end
-  def update_work_log
-    raise Exception, CUSTOM_ERROR_MESSAGE
-  end
-
   def destroy
     @task_template = current_templates.detect { |template| template.id == params[:id].to_i }
     @task_template.destroy
