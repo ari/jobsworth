a0c651ae5a1666124e55f64b2c205c8d2f61b60f
byakugie <nugi@prawirasoft.com>
2010-12-24 10:02:56 +0700

    modify gantt UI : truncate series name and make series name column wider

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -94,4 +94,15 @@ EOF
            })
   end
 
+  def link_to_gantt_task(task)
+    link = "<strong>#{task.issue_num}</strong> "
+    url = url_for(:id => task.task_num, :controller => 'tasks', :action => 'edit')
+    title = task.to_tip(:duration_format => current_user.duration_format,
+                        :workday_duration => current_user.workday_duration,
+                        :days_per_week => current_user.days_per_week,
+                        :user => current_user)
+    link += link_to(truncate(task.name, :length => 70), url, {:class => "tooltip tasklink", :title => title})
+    return link.html_safe
+  end
+
 end
