9ae3db563615429ecae15e42aca4d2625c69b71c
Ari Maniatis <ari@ish.com.au>
2010-12-24 13:30:40 +1100

    Clean up display of comment notification users.

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -6,7 +6,7 @@ class EmailAddress < ActiveRecord::Base
   has_and_belongs_to_many :abstract_tasks, :join_table=>'email_address_tasks', :association_foreign_key=>'task_id'
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   
-  def username_and_email
-    user ? "#{user.name} (#{self.email})" : self.email 
+  def username_or_email
+    user ? user.name : self.email 
   end
 end
