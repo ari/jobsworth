c28c6daec14644a319956f263611e1422b5bba83
byakugie <nugi@prawirasoft.com>
2011-01-12 13:00:15 +0700

    refactoring Email Delivery and add rescue block to deliver method, so EmailDelivery.cron will continue to send other emails. don't mark the delivery as 'sent' if fail to deliver

--- app/models/email_delivery.rb
+++ app/models/email_delivery.rb
@@ -6,18 +6,23 @@ class EmailDelivery < ActiveRecord::Base
   # this method will send all undelivered work log notifications
   # it should be called regularly in production environment
   def EmailDelivery.cron
-    EmailDelivery.select(:status=>'queued').includes(:work_log).each{|delivery|
-    	if delivery.work_log.log_type == EventLog::TASK_CREATED
-				Notifications.created(delivery)
-			else
-				Notifications.changed(delivery)
-			end
-    	delivery.status = 'sent'
-      delivery.save!
+    EmailDelivery.where(:status=>'queued').includes(:work_log).each{|delivery|
+      delivery.deliver
     }
   end
   
-  
+  def deliver
+    work_log = self.work_log
+    if work_log.log_type == EventLog::TASK_CREATED
+      Notifications.created(self).deliver
+    else
+      Notifications.changed(self).deliver
+    end
+    self.status = 'sent'
+    self.save!
+  rescue Exception => exc
+    logger.error "Failed to send notification delivery##{self.id}. Error : #{exc}"
+  end
 end
 
 # == Schema Information
