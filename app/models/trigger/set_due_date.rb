76cd2c138b728b54e9383b77f9e168d8ba89dd25
Elena Dzisyuk <dzisyuk.elen@gmail.com>
2011-03-25 13:47:39 +0200

    Fix trigger due_date to utc.

--- app/models/trigger/set_due_date.rb
+++ app/models/trigger/set_due_date.rb
@@ -10,7 +10,7 @@ class Trigger::SetDueDate < Trigger::Action
   end
 
   def execute(task)
-    task.due_at = Time.now + days.days
+    task.due_at = Time.now.utc + days.days
     task.save!
     return  "- Due: #{task.due_at.strftime_localized("%A, %d %B %Y")}\n".html_safe
   end
