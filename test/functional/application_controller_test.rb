49783a07a52dbf7d52c778c1c797abe2f254679e
Ivan Dzyzenko <intale.a@gmail.com>
2011-04-13 16:32:57 +0300

    fixed application controller test

--- test/functional/application_controller_test.rb
+++ test/functional/application_controller_test.rb
@@ -5,37 +5,35 @@ class ApplicationControllerTest < ActionController::TestCase
 
   fixtures :users, :companies, :customers, :tasks, :projects, :milestones, :work_logs
 
-  def setup
-    login
-  end
+  signed_in_admin_context do
 
-  test "should get current_user" do
+  should "get current_user" do
      get :index
      assert_equal users(:admin), @controller.current_user
   end
 
-  test "user 1 should be admin" do
+  should "user 1 be an admin" do
      get :index
      assert assigns(:current_user).admin?
   end
 
-  test "user 2 should NOT be admin" do
+  should "user 2 NOT to be an admin" do
     user = users(:fudge)
     user.company.create_default_statuses
-
+    sign_in user
     @request.session[:user_id] = user.id
     get :index
     assert !assigns(:current_user).admin?
   end
 
-  test "parse_time should handle 1w2d3h4m" do
+  should "parse_time to handle 1w2d3h4m" do
      get :index
      assert_equal 200040, @controller.parse_time("1w2d3h4m")
      assert_equal 240, @controller.parse_time("4m")
      assert_equal 27000, @controller.parse_time("1d")
   end
 
-  test "clients menu item should show for non admin users with read client option" do
+  should "clients menu item to be showed for non admin users with read client option" do
     user = users(:admin)
     user.update_attributes(:read_clients => true)
     user.admin=false
@@ -45,7 +43,7 @@ class ApplicationControllerTest < ActionController::TestCase
     assert_tag(:a, :attributes => { :href => "/clients/list" })
   end
 
-  test "clients menu item should not show for non admin users without read client option" do
+  should "clients menu item to be not showed for non admin users without read client option" do
     user = users(:admin)
     user.update_attributes(:read_clients => false)
     user.admin=false
@@ -55,20 +53,21 @@ class ApplicationControllerTest < ActionController::TestCase
     assert_no_tag(:a, :attributes => { :href => "/clients/list" })
   end
 
-  test "should never redirect back to url with ?format=js" do
+  should "never redirect back to url with ?format=js" do
     session[:history] = [ "/tasks/list?format=js" ]
     get :redirect_from_last
     assert_redirected_to "/tasks/list?"
   end
 
-  test "should render auto_complete_for_user_name" do
+  should "render auto_complete_for_user_name" do
     get :auto_complete_for_user_name, :user => { :name => "aaa" }
     assert_response :success
   end
 
-  test "should render auto_complete_for_customer_name" do
+  should "render auto_complete_for_customer_name" do
     get :auto_complete_for_customer_name, :customer => { :name => "aaa" }
     assert_response :success
   end
 
+end
 end
