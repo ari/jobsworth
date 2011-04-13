4a5f2ca770967f1d426e913d1eb12037483b7bfa
Ivan Dzyzenko <intale.a@gmail.com>
2011-04-13 12:07:43 +0300

    added migration from uncrypted passwords

--- db/migrate/20110411162011_delete_useless_column.rb
+++ db/migrate/20110411162011_delete_useless_column.rb
@@ -1,9 +1,19 @@
 class DeleteUselessColumn < ActiveRecord::Migration
   def self.up
+    #require 'devise/encryptors/bcrypt'
+
+  User.all.each do |user|
+    user.password = user[:password]
+    user.password_confirmation = user[:password]
+    user.save(:validate => false)
+  end
+
   remove_column :users, :email
   remove_column :users, :password
   end
 
   def self.down
+    add_column :users, :password, :string
+    add_column :users, :email, :string
   end
 end
