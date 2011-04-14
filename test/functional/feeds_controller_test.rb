ae0f37458cb78feb8f3d4055d9508b9401325064
Ivan Dzyzenko <intale.a@gmail.com>
2011-04-14 18:08:08 +0300

    fuxed feeds_controller test

--- test/functional/feeds_controller_test.rb
+++ test/functional/feeds_controller_test.rb
@@ -2,24 +2,24 @@ require "test_helper"
 
 class FeedsControllerTest < ActionController::TestCase
   fixtures :all
-  
+signed_in_admin_context do
   def setup 
     @request.host = 'cit.local.host'
   end
   
-  test "rss should render" do
+  should "render rss" do
     user = User.first
     get :rss, { :id => user.uuid }
     assert_response :success
   end 
 
-  test "ical should render" do
+  should "render ical" do
     user = User.first
     get :ical, { :id => user.uuid }
     assert_response :success
   end 
   
-  test "should be able to unsubscribe" do 
+  should "be able to unsubscribe" do
     user = User.find_by_uuid('1234567890abcdefghijklmnopqrstuv')
     assert_equal 1, user.newsletter
     get :unsubscribe, { :id => '1234567890abcdefghijklmnopqrstuv'}
@@ -28,7 +28,7 @@ class FeedsControllerTest < ActionController::TestCase
     assert_response :success
     assert @response.body.index("unsubscribed")
   end
-
+end
 #   xtest "should get RSS"
   
 #   xtest "should get iCal"
