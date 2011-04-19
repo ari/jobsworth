152120b90dbc00a67cff2f48940c3d2a9fcc4ef7
Ivan Dzyzenko <intale.a@gmail.com>
2011-04-19 13:37:47 +0300

    fixed schema.rb version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110413121531) do
+ActiveRecord::Schema.define(:version => 20110418071351) do
 
   create_table "access_levels", :force => true do |t|
     t.string   "name"
