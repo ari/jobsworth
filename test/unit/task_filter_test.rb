0ad0512328d72b5fcf290f0e2966e52db780f32a
Elena Dzisyuk <dzisyuk.elen@gmail.com>
2011-04-20 12:19:18 +0300

    Added test for the calendar, when task has a milestone.

--- test/unit/task_filter_test.rb
+++ test/unit/task_filter_test.rb
@@ -256,6 +256,25 @@ class TaskFilterTest < ActiveSupport::TestCase
         params= {:start=>(Time.now + 2.day).to_i, :end=> (Time.now + 7.day).to_i}
         assert_equal @filter.tasks_for_fullcalendar(params), [@t2]
       end
+      context "when the task has a milestone and the milestone's due_date not nil" do
+        setup do
+          @t2.milestone= Milestone.make(:project=> @t2.project, :company=> @t2.company)
+          @t2.milestone.due_at= @t2.due_at
+          @params= {:start=>(Time.now + 2.day).to_i, :end=> (Time.now + 7.day).to_i}
+        end
+         context "and tast has due_at," do
+           should "be task in the calendar" do
+              assert_equal [@t2], @filter.tasks_for_fullcalendar(@params)
+           end
+         end
+         context "and task has not due_at," do
+           should "be task in the calendar" do
+              @t2.due_at=nil
+              @t2.save!
+              assert_equal [@t2], @filter.tasks_for_fullcalendar(@params)
+           end
+         end
+      end
     end
   end
 end
