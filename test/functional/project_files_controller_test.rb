7cdaf9c141c4d3a19fa1ca46c576702f5888955e
Ivan Dzyzenko <intale.a@gmail.com>
2011-04-21 15:59:27 +0300

    let's checkout dependencies for peperclip

--- test/functional/project_files_controller_test.rb
+++ test/functional/project_files_controller_test.rb
@@ -31,7 +31,8 @@ class ProjectFilesControllerTest < ActionController::TestCase
                            :user_id => @user.id,
                            :file => @uploaded,
                            :uri => "450fc241fab7867e96536903244087f4")
-    @task.attachments.first.event_logs.new.save(:validate=>false)
+
+    puts system("convert")
     assert_equal true, File.exists?("#{Rails.root}/store/450fc241fab7867e96536903244087f4_original.png")
     assert_equal true, File.exists?("#{Rails.root}/store/450fc241fab7867e96536903244087f4_thumbnail.png")
 
@@ -43,7 +44,6 @@ class ProjectFilesControllerTest < ActionController::TestCase
   end
 
   should "not delete the file on disk if other tasks are linked to the same file" do
-    #Paperclip::Attachment({:styles=>{ :thumbnail=>"124x124"}, :path => "#{Rails.root}/store/:normalized_file_name.:extension"})
     @f=File.new("#{Rails.root}/test/fixtures/files/suri cruise.jpg", 'r')
     @temp=Tempfile.new("temp_#{Time.now}")
     @temp.write(@f.read)
@@ -55,7 +55,7 @@ class ProjectFilesControllerTest < ActionController::TestCase
                            :user_id => @user.id,
                            :file => @uploaded,
                            :uri => "8e732963114deed0079975414a0811b3")
-    @task.attachments.first.event_logs.new.save(:validate=>false)
+
     @second_task = Task.last
     @second_task.users << @second_task.company.users
     @second_task.save!
@@ -65,7 +65,7 @@ class ProjectFilesControllerTest < ActionController::TestCase
                            :user_id => @user.id,
                            :file => @uploaded,
                            :uri => "8e732963114deed0079975414a0811b3")
-    @second_task.attachments.first.event_logs.new.save(:validate=>false)
+
     assert_equal true, File.exists?("#{Rails.root}/store/8e732963114deed0079975414a0811b3_original.jpg")
     assert_equal true, File.exists?("#{Rails.root}/store/8e732963114deed0079975414a0811b3_thumbnail.jpg")
 
