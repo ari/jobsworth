3f8b376e75b193804607d534573f806e3235baa8
Ivan Dzyzenko <intale.a@gmail.com>
2011-04-21 11:55:57 +0300

    trying to fix project_files controller test. step2

--- test/functional/project_files_controller_test.rb
+++ test/functional/project_files_controller_test.rb
@@ -21,11 +21,15 @@ class ProjectFilesControllerTest < ActionController::TestCase
 
   should "delete the file on disk if other tasks aren't linked to the same file" do
     @f=File.new("#{Rails.root}/test/fixtures/files/rails.png", 'r')
+    @temp=Tempfile.new("temp_#{Time.now}")
+    @temp.write(@f.read)
+    head="Content-Disposition: form-data; name=\"tmp_files[]\"; filename=\"rails.png\" Content-Type: image/png "
+    @uploaded=ActionDispatch::Http::UploadedFile.new(:tempfile => @temp, :filename=>"rails.png", :type=>"image/png", :head => head)
     @task.attachments.make(:company => @user.company,
                            :customer => @task.project.customer,
                            :project => @task.project,
                            :user_id => @user.id,
-                           :file => ActionDispatch::Http::UploadedFile.new(:tempfile => @f, :filename=>"rails.png", :type=>"png", :head => "HEAD"),
+                           :file => @uploaded,
                            :uri => "450fc241fab7867e96536903244087f4")
     assert_equal true, File.exists?("#{Rails.root}/store/450fc241fab7867e96536903244087f4_original.png")
     assert_equal true, File.exists?("#{Rails.root}/store/450fc241fab7867e96536903244087f4_thumbnail.png")
@@ -39,11 +43,15 @@ class ProjectFilesControllerTest < ActionController::TestCase
 
   should "not delete the file on disk if other tasks are linked to the same file" do
     @f=File.new("#{Rails.root}/test/fixtures/files/suri cruise.jpg", 'r')
+    @temp=Tempfile.new("temp_#{Time.now}")
+    @temp.write(@f.read)
+    head="Content-Disposition: form-data; name=\"tmp_files[]\"; filename=\"suri cruise.jpg\" Content-Type: image/png "
+    @uploaded=ActionDispatch::Http::UploadedFile.new(:tempfile => @temp, :filename=>"suri cruise.jpg", :type=>"image/jpeg", :head => head)
     @task.attachments.make(:company => @user.company,
                            :customer => @task.project.customer,
                            :project => @task.project,
                            :user_id => @user.id,
-                           :file => ActionDispatch::Http::UploadedFile.new(:tempfile => @f, :filename=>"suri cruise.jpg", :type=>"jpeg", :head => "HEAD"),
+                           :file => @uploaded,
                            :uri => "8e732963114deed0079975414a0811b3")
 
     @second_task = Task.last
@@ -53,7 +61,7 @@ class ProjectFilesControllerTest < ActionController::TestCase
                            :customer => @second_task.project.customer,
                            :project => @second_task.project,
                            :user_id => @user.id,
-                           :file => ActionDispatch::Http::UploadedFile.new(:tempfile => @f, :filename=>"suri cruise.jpg", :type=>"jpeg", :head => "HEAD"),
+                           :file => @uploaded,
                            :uri => "8e732963114deed0079975414a0811b3")
 
     assert_equal true, File.exists?("#{Rails.root}/store/8e732963114deed0079975414a0811b3_original.jpg")
