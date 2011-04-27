8db22f84e880a6822d418504d6b3330e433b26a7
Ivan Dzyzenko <intale.a@gmail.com>
2011-04-27 16:55:42 +0300

    fixed migration from password to encrypted password

--- db/migrate/20110411162011_migration_from_password_to_ecnryted_password.rb
+++ db/migrate/20110411162011_migration_from_password_to_ecnryted_password.rb
@@ -1,15 +1,18 @@
 class MigrationFromPasswordToEcnrytedPassword < ActiveRecord::Migration
   def self.up
-  User.all.each do |user|
-    user.password = user[:password]
-    user.password_confirmation = user[:password]
-    user.save(:validate => false)
+    ActionDispatch::Callbacks.new(Proc.new {}, false).call({}) #Before actions below, we must reload environment
+    User.all.each do |user|
+      if user[:password] == nil
+        user[:password]=""
+      end
+    salt=Devise::Encryptors::Ssha.salt(Devise.stretches)
+    user[:password_salt]=salt
+    user[:encrypted_password]=Devise::Encryptors::Ssha.digest(user[:password], Devise.stretches,salt, Devise.pepper)
+    user.save(:validate=>false)
   end
-
-  remove_column :users, :password
+    remove_column :users, :password
   end
-
   def self.down
-    add_column :users, :password, :string
+   add_column :users, :password, :string
   end
 end
