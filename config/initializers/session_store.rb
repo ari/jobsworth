1c4bebe3569561b5df9e7a5297fd12f6cbaaad6c
Ivan Dzyzenko <intale.a@gmail.com>
2011-05-03 13:52:26 +0300

    now session stores in db

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, :key => '_clockingit_session', :expire_after => 3.hours
+Rails.application.config.session_store :active_record_store,
+                                       :key => '_jobsworth_session',
+                                       :expire_after => 3.hours
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
