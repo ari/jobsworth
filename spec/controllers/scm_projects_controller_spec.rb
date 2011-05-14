b452d47eaa7e0d74ac41a610b93c738f0400bb3b
Juan Hawa <juan.hawa@gmail.com>
2011-05-13 23:06:28 -0400

    Got green on Scm Projects controller spec

--- spec/controllers/scm_projects_controller_spec.rb
+++ spec/controllers/scm_projects_controller_spec.rb
@@ -1,55 +1,79 @@
 require 'spec_helper'
+
 describe ScmProjectsController do
+
   before(:each) do
-    @scm_project= mock_model(ScmProject)
+   @scm_project= mock_model(ScmProject)
   end
-  describe "GET new" do
-    it "should render new template" do
-      login_user( 'admin?' => true )
-      ScmProject.should_receive(:new).and_return(@scm_project)
+
+  describe "GET 'new'" do
+    it "should render the right template" do
+      user = User.make!(:admin => 1)
+      sign_in user
       get :new
       response.should render_template('scm_projects/new')
     end
+
     it "should redirect to last url, if user not have create project permission" do
-      login_user('admin?' => false )
+      user = User.make!(:admin => 0)
+      sign_in user
       get :new
       response.should  be_redirect
     end
   end
+
   describe "POST create" do
+
     context "user with admin permission" do
+
       before(:each) do
-        login_user( 'admin?' => true, "company"=> 1)
+        @user = User.make!(:admin => 1)
+        sign_in @user
       end
+
       context "with valiad params" do
+
         before(:each) do
-          ScmProject.should_receive(:new).with('these' => 'params').and_return(@scm_project = mock_model(ScmProject, { :save=>true}))
-          @scm_project.should_receive("company=").with(1)
-          post :create, :scm_project => { :these=>'params'}
+          ScmProject.should_receive(:new).
+            with('these' => 'params').
+            and_return(@scm_project = mock_model(ScmProject, { :save=>true}))
+          @scm_project.should_receive("company=").with(@user.company)
         end
+
         it "should redirect to show action" do
+          post :create, :scm_project => { :these => 'params'}
           response.should redirect_to(scm_project_url(@scm_project))
         end
       end
+
       context "with invalid params" do
+
         before(:each) do
-          ScmProject.should_receive(:new).with('these'=>'params').and_return(@scm_project = mock_model(ScmProject,{ :save=>false}))
-          @scm_project.should_receive("company=").with(1)
-          post :create,:scm_project =>  { :these=>'params'}
+          ScmProject.should_receive(:new).
+            with('these'=>'params').
+            and_return(@scm_project = mock_model(ScmProject,{ :save=>false}))
+          @scm_project.should_receive("company=").with(@user.company)
         end
+
         it "should render new template" do
+          post :create,:scm_project =>  { :these =>'params'}
           response.should render_template('scm_projects/new')
         end
       end
     end
+
     context "user without admin permission" do
+
       before(:each) do
-        login_user('admin?' => false )
-        post :create, :scm_project =>  { :these=>'params' }
+        user = User.make!(:admin => 0)
+        sign_in user
       end
+
       it "should redirect to last" do
+        post :create, :scm_project =>  { :these=>'params' }
         response.should be_redirect
       end
+
       it "should not create scm project" do
         ScmProject.should_not_receive(:new)
       end
