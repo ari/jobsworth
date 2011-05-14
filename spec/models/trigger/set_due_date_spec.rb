0a13da0ef84cb39849c37f15375113c49c693e98
Juan Hawa <juan.hawa@gmail.com>
2011-05-13 23:53:09 -0400

    Got green on Trigger::SetDueDate specs

--- spec/models/trigger/set_due_date_spec.rb
+++ spec/models/trigger/set_due_date_spec.rb
@@ -1,40 +1,31 @@
 require 'spec_helper'
+
 describe Trigger::SetDueDate do
-  before(:all) do
+
+  before(:each) do
     @action = Trigger::SetDueDate.new
-    @task= Task.make
+    @task   = Task.make!
   end
+
   it "should set task's due date in days(e.g. 3) from today" do
-    @action.days=4
+    @action.days = 4
     @action.execute(@task)
-    @task.due_at.to_date.should == (Time.now + 4.days).to_date
+    @task.due_at.to_date.should == (Time.now.utc + 4.days).to_date
   end
+
   it "should set due date event if task already has due date" do
-    @task.due_at = Time.now + 12.days
+    @task.due_at = Time.now.utc + 12.days
     @task.save!
-    @action.days=5
+    @action.days = 5
     @action.execute(@task)
-    @task.due_at.to_date.should == (Time.now + 5.days).to_date
+    @task.due_at.to_date.should == (Time.now.utc + 5.days).to_date
   end
+
   it "should save task" do
-    @action.days=5
+    @action.days = 5
     @action.execute(@task)
-    @task.due_at.to_date.should == (Time.now + 5.days).to_date
+    @task.due_at.to_date.should == (Time.now.utc + 5.days).to_date
     @task.reload
-    @task.due_at.to_date.should == (Time.now + 5.days).to_date
+    @task.due_at.to_date.should == (Time.now.utc + 5.days).to_date
   end
 end
-
-# == Schema Information
-#
-# Table name: trigger_actions
-#
-#  id         :integer(4)      not null, primary key
-#  trigger_id :integer(4)
-#  name       :string(255)
-#  type       :string(255)
-#  argument   :integer(4)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
