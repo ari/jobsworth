351a084dfe91e8292b8536bcbba2629eaa216bd2
Juan Hawa <juan.hawa@gmail.com>
2011-05-18 13:27:33 -0400

    Fixed select list on 'new' and 'edit' views

--- app/models/score_rule_types.rb
+++ app/models/score_rule_types.rb
@@ -1,5 +1,4 @@
 module ScoreRuleTypes
-
   FIXED             = 1
   TASK_AGE          = 2
   LAST_COMMENT_AGE  = 3
@@ -7,5 +6,12 @@ module ScoreRuleTypes
 
   def self.all_score_types
     [FIXED, TASK_AGE, LAST_COMMENT_AGE, OVERDUE] 
+  end
+
+  def self.to_select_list
+    { 'Fixed'             => FIXED, 
+      'Task Age'          => TASK_AGE,
+      'Last Comment Age'  => LAST_COMMENT_AGE,
+      'Overdue'           => OVERDUE }
   end
 end
