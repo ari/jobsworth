90eddf3a14886b7e19ffe0dc78fbc62f0adeb82e
Juan Hawa <juan.hawa@gmail.com>
2011-05-21 11:03:46 -0400

    Got green on Scm Changesets Controller

--- spec/controllers/scm_changesets_controller_spec.rb
+++ spec/controllers/scm_changesets_controller_spec.rb
@@ -1,16 +1,30 @@
 require 'spec_helper'
+include Devise::TestHelpers
 
 describe ScmChangesetsController do
-  describe "POST create" do
+
+  before(:each) do
+    sign_in User.make
+  end
+
+  describe "POST 'create'" do
     context "with valid params" do
+
       before(:each) do
-        ScmChangeset.should_receive(:create_from_web_hook).with("scm_changeset"=>{ 'these'=> :params }, "action"=>"create", "controller"=>"scm_changesets").and_return(mock_model(ScmChangeset))
-        post :create, :scm_changeset=>{ :these=> :params }
+        ScmChangeset.should_receive(:create_from_web_hook).
+                     with("scm_changeset" =>  { 'these'=> :params }, 
+                          "action"        =>  "create", 
+                          "controller"    =>  "scm_changesets").
+                     and_return(mock_model(ScmChangeset))
+
+        post :create, :scm_changeset => { :these=> :params }
       end
+
       it "respond with HTTP-STATUS: 201 CREATED" do
         response.status.should == 201
       end
     end
+
     context "with invalid params" do
       before(:each) do
         ScmChangeset.should_receive(:create_from_web_hook).with("scm_changeset"=>{ 'these'=> :params }, "action"=>"create", "controller"=>"scm_changesets").and_return(false)
