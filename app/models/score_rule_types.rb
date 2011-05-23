c084ec4c240b1c7e057962745a9d3be3727801f4
Juan Hawa <juan.hawa@gmail.com>
2011-05-23 14:22:55 -0400

    The 'index' view for the score rules, now shows the Score Type as an string not a number

--- app/models/score_rule_types.rb
+++ app/models/score_rule_types.rb
@@ -12,4 +12,12 @@ module ScoreRuleTypes
       'Task Age'          => TASK_AGE,
       'Last Comment Age'  => LAST_COMMENT_AGE }
   end
+
+  def self.get_name_of(score_rule_type)
+    case score_rule_type
+      when FIXED then 'Fixed'
+      when TASK_AGE then 'Task Age'
+      when 'LAST_COMMENT_AGE' then 'Last Comment Age'
+    end
+  end
 end
