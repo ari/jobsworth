122b7ad701bd4b6d7a80a3e83f50da8b122c807b
Juan Hawa <juan.hawa@gmail.com>
2011-05-25 15:52:11 -0400

    Fixed score type text

--- app/models/score_rule_types.rb
+++ app/models/score_rule_types.rb
@@ -9,7 +9,7 @@ module ScoreRuleTypes
   end
 
   def self.to_select_list
-    { 'Fixed'                                     => FIXED, 
+    { 'Fixed score'                               => FIXED, 
       'Score per day since creation'              => TASK_AGE,
       'Score per day since last public comment'   => LAST_COMMENT_AGE,
       'Score per day overdue'                     => OVERDUE }
@@ -17,7 +17,7 @@ module ScoreRuleTypes
 
   def self.get_name_of(score_rule_type)
     case score_rule_type
-      when FIXED              then 'Fixed'
+      when FIXED              then 'Fixed score'
       when TASK_AGE           then 'Score per day since creation'
       when LAST_COMMENT_AGE   then 'Score per day since last public comment'
       when OVERDUE            then 'Score per day overdue'
