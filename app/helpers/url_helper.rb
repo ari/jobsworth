de45356bedb12b52e37ea69334ae9c9432becff9
Ari Maniatis <ari@ish.com.au>
2011-05-28 13:42:33 +1000

    Oops, forgot the module wrapper.

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,5 +1,8 @@
+module UrlHelper
 
-# When devise sends emails (such as password resets), use the correct sub-domain
-def set_mailer_url_options
-  ActionMailer::Base.default_url_options[:host] = with_subdomain(request.subdomain)
+  # When devise sends emails (such as password resets), use the correct sub-domain
+  def set_mailer_url_options
+    ActionMailer::Base.default_url_options[:host] = with_subdomain(request.subdomain)
+  end
+
 end
