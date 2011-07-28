867f52108ae08e2513200cd81071b8a2f82d3b6c
Elena Dzisyuk <dzisyuk.elen@gmail.com>
2011-07-28 17:02:25 +0300

    Enabled assets for Rails 3.1.

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,7 @@ module Jobsworth
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    config.assets.enabled = true
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
