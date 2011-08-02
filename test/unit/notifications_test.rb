43195ebd6fcadf5a873c9322877405b5b3952e1a
Elena Dzisyuk <dzisyuk.elen@gmail.com>
2011-08-02 13:57:10 +0300

    Fixed notifications_test for Rail 3.1.

--- test/unit/notifications_test.rb
+++ test/unit/notifications_test.rb
@@ -45,7 +45,7 @@ class NotificationsTest < ActiveRecord::TestCase
         @task.company.properties.each{ |p|
           @task.set_property_value(p, p.default_value)
         }
-        notification = Notifications.create_created(@deliveries.first)
+        notification = Notifications.created(@deliveries.first)
         assert_equal @task.users_to_notify(@user).map(&:email), [@user.email]
         assert @user.can_view_task?(@task)
         assert_match /tasks\/view/, notification.body.to_s
@@ -57,7 +57,7 @@ class NotificationsTest < ActiveRecord::TestCase
         @expected['Mime-Version'] = '1.0'
         @expected.body    = read_fixture('changed')
         @work_log.update_attributes(:log_type => EventLog::TASK_COMPLETED, :body => "Task Changed")
-        notification = Notifications.create_changed(@deliveries.first)
+        notification = Notifications.changed(@deliveries.first)
         assert @user.can_view_task?(@task)
         assert_match  /tasks\/view/,  notification.body.to_s
         assert_equal @expected.body.to_s, notification.body.to_s
@@ -66,13 +66,13 @@ class NotificationsTest < ActiveRecord::TestCase
       should "not escape html in email" do
         html = '<strong> HTML </strong> <script type = "text/javascript"> alert("XSS");</script>'
         @work_log.update_attributes(:log_type => EventLog::TASK_MODIFIED, :body => html)
-        notification = Notifications.create_changed(@deliveries.first)
+        notification = Notifications.changed(@deliveries.first)
         assert_not_nil notification.body.to_s.index(html)
       end
 
       should "should have 'text/plain' context type" do
         @work_log.update_attributes(:log_type => EventLog::TASK_MODIFIED, :body => "Task changed")
-        notification = Notifications.create_changed(@deliveries.first)
+        notification = Notifications.changed(@deliveries.first)
         assert_match /text\/plain/, notification.content_type
       end
 
@@ -89,14 +89,14 @@ class NotificationsTest < ActiveRecord::TestCase
       should "create changed mail without view task link" do
         @work_log = WorkLog.make(:user => @user, :task => @task, :log_type => EventLog::TASK_COMPLETED, :body => "Task Changed")
         @delivery = @work_log.email_deliveries.make(:email => @user.email, :user=>@user)
-        notification = Notifications.create_changed(@delivery)
+        notification = Notifications.changed(@delivery)
         assert_nil notification.body.to_s.index("/tasks/view/")
       end
 
       should "create created mail without view task link" do
         @work_log = WorkLog.make(:user => @user, :task => @task, :log_type => EventLog::TASK_CREATED)
         @delivery = @work_log.email_deliveries.make(:email=> @user.email, :user=>@user)
-        notification = Notifications.create_created(@delivery)
+        notification = Notifications.created(@delivery)
         assert_nil notification.body.to_s.index("/tasks/view/")
       end
     end
