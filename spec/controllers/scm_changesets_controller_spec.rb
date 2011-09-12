0eb1441705885e31dbe0c90ba3d97a4e45dfca4d
asymmetric <asymmetric@autistici.org>
2011-09-13 01:39:41 +0200

    Must pass params to `with' as string, not symbols

--- spec/controllers/scm_changesets_controller_spec.rb
+++ spec/controllers/scm_changesets_controller_spec.rb
@@ -11,8 +11,8 @@ describe ScmChangesetsController do
 
       before(:each) do
         ScmChangeset.should_receive(:create_from_web_hook).
-                     with("scm_changeset" =>  { 'these'=> :params }, 
-                          "action"        =>  "create", 
+                     with("scm_changeset" =>  { "these" => "params" },
+                          "action"        =>  "create",
                           "controller"    =>  "scm_changesets").
                      and_return(mock_model(ScmChangeset))
 
@@ -26,9 +26,16 @@ describe ScmChangesetsController do
 
     context "with invalid params" do
       before(:each) do
-        ScmChangeset.should_receive(:create_from_web_hook).with("scm_changeset"=>{ 'these'=> :params }, "action"=>"create", "controller"=>"scm_changesets").and_return(false)
-        post :create, :scm_changeset=>{ :these=> :params }
+        ScmChangeset.
+          should_receive(:create_from_web_hook).
+          with("scm_changeset" => { "these" => "params" },
+               "action" => "create",
+               "controller" => "scm_changesets").
+          and_return(false)
+
+        post :create, :scm_changeset=>{ :these=> "params" }
       end
+
       it "respond with HTTP-STATUS: 422 Unprocessable Entity" do
         response.status.should == 422
       end
