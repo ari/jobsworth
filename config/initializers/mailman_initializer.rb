bfdce95f3a6b2d0baae521594a2a70507fe190e2
asymmetric <asymmetric@autistici.org>
2011-09-23 16:36:01 +0200

    add more constants for the mailman

--- config/initializers/mailman_initializer.rb
+++ config/initializers/mailman_initializer.rb
@@ -1,1 +1,2 @@
-MAILMAN_BAD_SUBJECTS = YAML.load_file(File.join(Rails.root, 'config/bad_subjects.yml'))['bad_subject'].collect { |s| s.strip }
+BAD_SUBJECTS = YAML.load_file(File.join(Rails.root, 'config/bad_subjects.yml'))['bad_subject'].collect { |s| s.strip }
+MAX_ATTACHMENT_SIZE = 5*1024*1024
