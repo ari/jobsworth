777fd1287ecbcc40cd052d17f70e0ff516b99a32
asymmetric <asymmetric@autistici.org>
2011-09-26 12:04:00 +0200

    Add null to bad_subjects.yml

--- config/initializers/mailman.rb
+++ config/initializers/mailman.rb
@@ -1,2 +1,2 @@
-BAD_SUBJECTS = YAML.load_file(File.join(Rails.root, 'config/bad_subjects.yml'))['bad_subject'].collect { |s| s.strip }
+BAD_SUBJECTS = YAML.load_file(File.join(Rails.root, 'config/bad_subjects.yml'))['bad_subject']
 MAX_ATTACHMENT_SIZE = 5*1024*1024
