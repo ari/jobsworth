9da90fd78cd21e193a9375a86c8488e044617be8
asymmetric <asymmetric@autistici.org>
2011-10-17 12:24:27 +0200

    locals must be passed as symbold, not strings

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,10 +1,14 @@
 # encoding: UTF-8
 module PropertiesHelper
   def add_value_link
-    link_to(_("Add"), "#", {"data-property" => render_to_string(
-						:partial => 'property_value',
-						:locals => { 'pv' => @property.property_values.build }),
-             :id=> 'add_value_link'})
+    link_to(_("Add"), "#", {
+      "data-property" => render_to_string(
+        partial: 'property_value',
+        locals: {
+          pv: @property.property_values.build
+        }),
+      id: 'add_value_link'
+    })
   end
 
 end
