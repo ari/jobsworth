6ebad6da4aa7ea8f4ef39d5ba1a4080c92c8fab5
liufengyun <liufengyunchina@gmail.com>
2011-11-14 10:09:08 +0800

    fix ssha encryption bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ require 'digest/md5'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :encryptable,
          :recoverable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model
