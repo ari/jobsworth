956fbed1bfdbdc1fc696bb43c934e14ca4f37641
liufengyun <liufengyunchina@gmail.com>
2011-11-16 11:30:40 +0800

    Add users of task customers to "users to notify" selection list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -348,6 +348,12 @@ class TasksController < ApplicationController
     excluded_ids = params[:watcher_ids].blank? ? 0 : params[:watcher_ids]
     @users = current_user.customer.users.active.where("id NOT IN (#{excluded_ids})").order('name').limit(50)
     @task = controlled_model.accessed_by(current_user).find_by_id(params[:id])
+
+    @task && @task.customers.each do |customer|
+      @users = @users + customer.users.active.where("id NOT IN (#{excluded_ids})").order('name').limit(50)
+    end
+    @users = @users.uniq.sort_by{|user| user.name}.first(50)
+
     if @task && current_user.customer != @task.project.customer
       @users = @users + @task.project.customer.users.active.where("id NOT IN (#{excluded_ids})")
       @users = @users.uniq.sort_by{|user| user.name}.first(50)
