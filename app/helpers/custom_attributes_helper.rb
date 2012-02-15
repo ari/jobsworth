17eeea6bc6e0040f2f39af27a30c10939f8efb79
liufengyun <liufengyunchina@gmail.com>
2012-02-15 17:12:14 +0800

    fix custom properties style

--- app/helpers/custom_attributes_helper.rb
+++ app/helpers/custom_attributes_helper.rb
@@ -7,7 +7,7 @@ module CustomAttributesHelper
   ###
   def link_to_add_attribute
     js = "appendPartial('/custom_attributes/fields', '#attributes')"
-    link_to_function(_("Add another attribute"), js, :class => "add_attribute")
+    link_to_function(_("Add another attribute"), js, :class => "add_attribute btn")
   end
 
   ###
