271558a1f09fe18424ba494b407b0e397ec68d1a
liufengyun <liufengyunchina@gmail.com>
2012-02-15 18:02:01 +0800

    fix task properties style

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 module PropertiesHelper
   def add_value_link
-    link_to(_("Add"), "#", {
+    link_to(_("Add Property Value"), "#", {
       "data-property" => render_to_string(
         partial: 'property_value',
         locals: {
