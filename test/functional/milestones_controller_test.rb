5a89f8b66216df2d3fdedb9037eb76122f35f292
liufengyun <liufengyunchina@gmail.com>
2012-04-11 20:05:32 +0800

    fix can't update milestone bug

--- test/functional/milestones_controller_test.rb
+++ test/functional/milestones_controller_test.rb
@@ -9,11 +9,31 @@ class MilestonesControllerTest < ActionController::TestCase
     @request.session[:user_id] = @user.id
     @user.company.create_default_statuses
   end
+
   context 'a normal milestone' do
     should "render get_milestones" do
       @task=Task.first
       get :get_milestones, :project_id => @task.project.id
       assert_response :success
+    end
+
+    should "be able to create milestone" do
+      get :new, :project_id => @user.projects.first.id
+      assert_response :success
+
+      post :create, :milestone => {:name => "test", :due_at => Time.now.ago(-3.days), :description => "test milestone", :project_id => Project.first.id}
+      assert_response 302
+    end
+
+    should "be able to update milestone" do
+      project = @user.projects.first
+      milestone = project.milestones.create!(:name => "test", :due_at => Time.now.ago(-3.days), :description => "test milestone", :company => @user.company)
+
+      get :edit, :id => milestone.id
+      assert_response :success
+
+      put :update, :id => milestone.id, :milestone => {:name => "test2"}
+      assert_response 302
     end
   end
 end
