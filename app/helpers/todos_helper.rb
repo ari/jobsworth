a486da81cd7d1d079a43cfea50a55506fcf7e526
liufengyun <liufengyunchina@gmail.com>
2012-10-05 14:41:20 +0800

    don't show tip for todo

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,18 +1,12 @@
 # encoding: UTF-8
 module TodosHelper
   def todo_open_close_check_box(todo)
-    title = _('Open <b>%s</b>', h(todo.name))
-    if todo.done?
-      title = _("Close <b>%s</b>", h(todo.name))
-    end
-
     url = "/todos/toggle_done/#{ todo.id }?task_id=#{ @task.id }"
     id = todo.id
 
-    check_box("todo", "done", { :title => title,
+    check_box("todo", "done", {
       :checked => todo.done?,
       :class => "button checkbox",
-      :rel => "tooltip",
       :id => "button_#{ id }"
     })
   end
