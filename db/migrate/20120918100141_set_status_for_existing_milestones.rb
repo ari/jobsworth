527441184c2688a3abf10f49eed66046de1344d5
liufengyun <liufengyunchina@gmail.com>
2012-11-15 23:55:36 +0800

    fix error in migration #394

--- db/migrate/20120918100141_set_status_for_existing_milestones.rb
+++ db/migrate/20120918100141_set_status_for_existing_milestones.rb
@@ -2,9 +2,9 @@ class SetStatusForExistingMilestones < ActiveRecord::Migration
   def up
     Milestone.all.each do |m|
       if m.completed_at.nil?
-        m.update_attributes(:status_name => :open)
+        m.update_column(:status_name => :open)
       else
-        m.update_attributes(:status_name => :closed)
+        m.update_column(:status_name => :closed)
       end
     end
   end
