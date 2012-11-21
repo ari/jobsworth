84645eb4c32dfacccf638c20977281923af5c102
liufengyun <liufengyunchina@gmail.com>
2012-11-21 16:41:17 +0800

    remove synchronize and autoresize from column picker

--- vendor/assets/javascripts/slickgrid/slick.columnpicker.js
+++ vendor/assets/javascripts/slickgrid/slick.columnpicker.js
@@ -40,27 +40,6 @@
             .appendTo($li);
       }
 
-      $("<hr/>").appendTo($menu);
-      $li = $("<li />").appendTo($menu);
-      $input = $("<input type='checkbox' />").data("option", "autoresize");
-      $("<label />")
-          .text("Force fit columns")
-          .prepend($input)
-          .appendTo($li);
-      if (grid.getOptions().forceFitColumns) {
-        $input.attr("checked", "checked");
-      }
-
-      $li = $("<li />").appendTo($menu);
-      $input = $("<input type='checkbox' />").data("option", "syncresize");
-      $("<label />")
-          .text("Synchronous resize")
-          .prepend($input)
-          .appendTo($li);
-      if (grid.getOptions().syncColumnCellResize) {
-        $input.attr("checked", "checked");
-      }
-
       $menu
           .css("top", e.pageY - 10)
           .css("left", e.pageX - 10)
@@ -68,25 +47,6 @@
     }
 
     function updateColumn(e) {
-      if ($(e.target).data("option") == "autoresize") {
-        if (e.target.checked) {
-          grid.setOptions({forceFitColumns:true});
-          grid.autosizeColumns();
-        } else {
-          grid.setOptions({forceFitColumns:false});
-        }
-        return;
-      }
-
-      if ($(e.target).data("option") == "syncresize") {
-        if (e.target.checked) {
-          grid.setOptions({syncColumnCellResize:true});
-        } else {
-          grid.setOptions({syncColumnCellResize:false});
-        }
-        return;
-      }
-
       if ($(e.target).is(":checkbox")) {
         var visibleColumns = [];
         $.each(columnCheckboxes, function (i, e) {
