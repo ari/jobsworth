be90b1214b9bbe7e5b98aa186c924ea203798046
liufengyun <liufengyunchina@gmail.com>
2012-12-12 22:12:52 +0800

    refactor user account page

---
+++ app/assets/javascripts/email_addresses.js
@@ -0,0 +1,87 @@
+var jobsworth = jobsworth || {};
+
+jobsworth.EmailAddresses = (function($) {
+  function EmailAddresses(user_id) {
+    this.user_id = user_id;
+
+    this.bindEvents();
+  }
+
+  EmailAddresses.prototype.bindEvents = function() {
+    var self = this;
+
+    $('#add-email-container button').click(function() {
+      var email = $('#add-email-container input#email').val();
+
+      if (!/^\S+@\S+\.\S+$/.test(email)) {
+        alert('Please input a valid email address.');
+        return false;
+      }
+
+      self.addEmail(email);
+
+      return false;
+    })
+
+    $('.email_address .set-default').live('click', function() {
+      var id = $(this).parents('.email_address').data('id');
+      self.setDefault(id);
+      return false;
+    })
+
+    $('.email_address .delete-link').live('click', function() {
+      if (confirm('Are you sure to delete the email address?')) {
+        var id = $(this).parents('.email_address').data('id');
+        self.deleteEmail(id);
+      }
+      return false;
+    })
+  }
+
+  EmailAddresses.prototype.deleteEmail = function(id) {
+    var jqXHR = $.post('/email_addresses/' + id, {_method: 'delete'}, function(res) {
+      if (res.success) {
+        $('.email_address[data-id=' + id + ']').remove();
+      } else {
+        alert(res.message);
+      }
+    })
+
+    jqXHR.error(function() {
+      alert("Sorry, there's an exception, please retry later.");
+    })
+  }
+
+  EmailAddresses.prototype.setDefault = function(id) {
+    var jqXHR = $.post('/email_addresses/' + id + '/default', {_method: 'put'}, function(res) {
+      if (res.success) {
+        $('.email_address').removeClass('default');
+        $('.email_address[data-id=' + id + ']').addClass('default');
+      } else {
+        alert(res.message);
+      }
+    })
+
+    jqXHR.error(function() {
+      alert("Sorry, there's an exception, please retry later.");
+    })
+  }
+
+  EmailAddresses.prototype.addEmail = function(email) {
+    var data = {user_id: this.user_id, email: email};
+    var jqXHR = $.post('/email_addresses', {email_address: data}, function(res) {
+      if (res.success) {
+        $('#emails-container').append(res.html);
+        $('#add-email-container input#email').val('');
+      } else {
+        alert(res.message);
+      }
+    })
+
+    jqXHR.error(function() {
+      alert("Sorry, there's an exception, please retry later.");
+    })
+  }
+
+  return EmailAddresses;
+})(jQuery)
--- app/controllers/email_addresses_controller.rb
+++ app/controllers/email_addresses_controller.rb
@@ -6,6 +6,34 @@ class EmailAddressesController < ApplicationController
     @email_addresses = current_user.company.email_addresses.where("user_id IS NULL").order("email ASC").paginate(:page => params[:page], :per_page => 50)
   end
 
+  def create
+    # try link orphaned email address first
+    @email_address = current_user.company.email_addresses.where(:email => params[:email_address][:email]).where('user_id IS NULL').first
+    @email_address ||= EmailAddress.new(params[:email_address])
+
+    # newly added email address can't be default
+    @email_address.default = false
+    @email_address.company_id = current_user.company_id
+
+    if @email_address.user != current_user and !current_user.admin?
+      return render :json => {:success => false, :message => "You're not authorized to perform the operation"}
+    end
+
+    # link to orhpaned email address
+    if !@email_address.new_record?
+      @email_address.link_to_user(params[:email_address][:user_id])
+      html = render_to_string :partial => 'email_addresses/email_address', :locals => {:email_address => @email_address}
+      return render :json => {:success => true, :html => html}
+    end
+
+    if @email_address.save
+      html = render_to_string :partial => 'email_addresses/email_address', :locals => {:email_address => @email_address}
+      return render :json => {:success => true, :html => html}
+    else
+      return render :json => {:success => false, :message => @email_address.errors.full_messages.join(', ') }
+    end
+  end
+
   def update
     @email_address = current_user.company.email_addresses.find(params[:id])
     @email_address.link_to_user(params[:email_address][:user_id])
@@ -17,4 +45,31 @@ class EmailAddressesController < ApplicationController
     @email_address = current_user.company.email_addresses.find(params[:id])
   end
 
+  def destroy
+    @email_address = current_user.company.email_addresses.find(params[:id])
+
+    if @email_address.user != current_user and !current_user.admin?
+      return render :json => {:success => false, :message => "You're not authorized to perform the operation"}
+    end
+
+    if @email_address.default?
+      render :json => {:success => false, :message => "Default email address can't be deleted."}
+    else
+      @email_address.destroy
+      render :json => {:success => true}
+    end
+  end
+
+  def default
+    @email_address = current_user.company.email_addresses.find(params[:id])
+
+    if @email_address.user != current_user and !current_user.admin?
+      return render :json => {:success => false, :message => "You're not authorized to perform the operation"}
+    end
+
+    @email_address.user.email_addresses.update_all(:default => false)
+    @email_address.update_column(:default, true)
+
+    render :json => {:success => true}
+  end
 end
