a5125bfed27380fd432553fab89d12ed3892352b
liufengyun <liufengyunchina@gmail.com>
2012-12-12 14:13:08 +0800

    fix assets migration error

--- db/migrate/20121210110131_move_assets_to_tenant_directory.rb
+++ db/migrate/20121210110131_move_assets_to_tenant_directory.rb
@@ -2,20 +2,16 @@ class MoveAssetsToTenantDirectory < ActiveRecord::Migration
   def up
     company = Company.first
 
-    dir = File.join($CONFIG[:store_root], company.id.to_s)
-    `mkdir -p #{dir}`
+    parent_dir = File.expand_path("..", $CONFIG[:store_root])
 
-    # project files
-    files = File.join($CONFIG[:store_root], "*.*")
-    `mv #{files} #{dir}`
+    # mv assets to temp dir
+    tmp_dir = File.join(parent_dir, File.basename($CONFIG[:store_root]) + ".tmp")
+    puts `mv #{$CONFIG[:store_root]} #{tmp_dir}`
 
-    # avatars
-    avatars = File.join($CONFIG[:store_root], "avatars")
-    `mv #{avatars} #{dir}`
-
-    # logos
-    logos = File.join($CONFIG[:store_root], "logos")
-    `mv #{logos} #{dir}`
+    # mv temp dir to dest dir
+    dest_dir = File.join($CONFIG[:store_root], company.id.to_s)
+    puts `mkdir  #{$CONFIG[:store_root]}`
+    puts `mv #{tmp_dir} #{dest_dir}`
   end
 
   def down
