3756a60e2a116bcb698fbfb2314185f156a4ba56
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-03-06 21:14:05 +0100

    Rewrite the lib/setup.rb script to a Thor task

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require File.expand_path('../environment.local.rb', __FILE__) if File.exist?(File.expand_path('../environment.local.rb', __FILE__))
+if File.exist?(File.expand_path('../environment.local.rb', __FILE__))
+  require File.expand_path('../environment.local.rb', __FILE__)
+end
 
 # Initialize the rails application
 Jobsworth::Application.initialize!
