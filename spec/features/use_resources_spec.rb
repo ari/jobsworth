29adac6bb76f30bd1a647b61ef064cadb0be19f7
boni <boni@twine.hu>
2013-03-24 21:09:50 +0100

    rspec use resource features udpated

--- spec/features/use_resources_spec.rb
+++ spec/features/use_resources_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe 'use resources' do
-  def login_as(username, password)
+  def login_as(user)
     visit root_path
-    fill_in "Username", :with => username
-    fill_in "Password", :with => password
+    fill_in "Username", :with => user.username
+    fill_in "Password", :with => user.password
+    find("#user_subdomain").set(admin.company.subdomain)
     click_button "Login"
   end
 
@@ -14,14 +15,14 @@ describe 'use resources' do
   let(:admin) { FactoryGirl.create(:admin, :company => company, :use_resources => user_use_resources) }
 
   it 'should display resources menu item if allowed' do
-    login_as(admin.username, admin.password)
+    login_as(admin)
     page.should have_content(/resources/i)
   end
 
   context "when user not use resources" do
     let(:user_use_resources) { false }
     it 'should not display resources menu item if not allowed' do
-      login_as(admin.username, admin.password)
+      login_as(admin)
       page.should_not have_content(/resources/i)
     end
   end
@@ -29,7 +30,7 @@ describe 'use resources' do
   context "when company not use resources" do
     let(:company_use_resources) {  false }
     it 'should not display resources menu item if not allowed' do
-      login_as(admin.username, admin.password)
+      login_as(admin)
       page.should_not have_content(/resources/i)
     end
   end
