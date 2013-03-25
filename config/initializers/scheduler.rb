65c298ad9cc24f9439516dc0492165e9d38c154f
Vikrant Chaudhary <vikrant@webstream.io>
2013-03-25 14:39:06 +0530

    Migrated script/scheduler.rb to config/initializers/scheduler.rb Scheduler now runs within the Rails process.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,11 +2,25 @@ require 'rufus/scheduler'
 
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.cron "*/1 * * * *" do
+# Every morning at 6:17am
+scheduler.cron '17 6 * * *' do
   Rails.logger.tagged "SCHEDULER" do
-    Rails.logger.info "If you're happy and you know it"
-    Rails.logger.info "And you really want to show it"
-    Rails.logger.info "Clap your hands"
-    Rails.logger.info Time.now.inspect
+    Rails.logger.info "Expire hide_until tasks"
+    TaskRecord.expire_hide_until
+  end
+end
+
+# Schedule tasks every 10 minutes
+scheduler.cron '*/10 * * * *' do
+  Rails.logger.tagged "SCHEDULER" do
+    User.schedule_tasks
+  end
+end
+
+# Every morning at 6:43am
+scheduler.cron '43 6 * * *' do
+  Rails.logger.tagged "SCHEDULER" do
+    Rails.logger.info "Recalculating score values for all the tasks"
+    TaskRecord.calculate_score
   end
 end
