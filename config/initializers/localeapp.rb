4a4ba198658939b0ce036fa78700565848a399d7
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-04-18 13:57:50 +0200

    Fix localeapp initializer

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,12 +1,12 @@
 if defined?(Localeapp)
   require 'localeapp/rails'
 
-  if Setting.localeapp.api_key.present?
+  if Setting.localeapp!.api_key.present?
     Localeapp.configure do |config|
       config.api_key = Setting.localeapp.api_key
-      config.sending_environments   = Setting.localeapp.sending_environments
-      config.polling_environments   = Setting.localeapp.polling_environments
-      config.reloading_environments = Setting.localeapp.reloading_environments
+      config.sending_environments   = Setting.localeapp.sending_environments.to_a
+      config.polling_environments   = Setting.localeapp.polling_environments.to_a
+      config.reloading_environments = Setting.localeapp.reloading_environments.to_a
     end
   end
 end
