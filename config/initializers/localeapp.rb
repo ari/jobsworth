c82d55a86b442e3e8b8adae45a5f8c3f2ce65984
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-04-18 11:28:06 +0200

    Ajust typo in localeapp initializer

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -4,8 +4,8 @@ if defined?(Localeapp)
   if Setting.localeapp.api_key.present?
     Localeapp.configure do |config|
       config.api_key = Setting.localeapp.api_key
-      config.sending_environments = Setting.localeapp.sending_environments
-      config.polling_environments = Setting.localeapp.polling_environments
+      config.sending_environments   = Setting.localeapp.sending_environments
+      config.polling_environments   = Setting.localeapp.polling_environments
       config.reloading_environments = Setting.localeapp.reloading_environments
     end
   end
