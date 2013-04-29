2622a4acab3ab5cfb22fc7f71c6bff2432e057f7
Vikrant Chaudhary <vikrant@webstream.io>
2013-04-29 22:15:13 +0530

    Allow disabling scheduler by an environment variable. Handy while building war file through warbler, as the database.yml contains a jndi connection which is not available in Jenkins. Fixes recent build errors.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,26 +1,45 @@
-require 'rufus/scheduler'
+module SchedulerInitializer
+  extend self
+  require 'rufus/scheduler'
 
-scheduler = Rufus::Scheduler.start_new
+  def scheduler_disabled?
+    ENV['JOBSWORTH_DISABLE_SCHEDULER'].present?
+  end
 
-# Every morning at 6:17am
-scheduler.cron '17 6 * * *' do
-  Rails.logger.tagged "SCHEDULER" do
-    Rails.logger.info "Expire hide_until tasks"
-    TaskRecord.expire_hide_until
+  def init
+    if scheduler_disabled?
+      Rails.logger.tagged "SCHEDULER" do
+        Rails.logger.info "Scheduler is disabled"
+      end
+      return
+    end
+
+    scheduler = Rufus::Scheduler.start_new
+
+    # Every morning at 6:17am
+    scheduler.cron '17 6 * * *' do
+      Rails.logger.tagged "SCHEDULER" do
+        Rails.logger.info "Expire hide_until tasks"
+        TaskRecord.expire_hide_until
+      end
+    end
+
+    # Schedule tasks every 10 minutes
+    scheduler.cron '*/10 * * * *' do
+      Rails.logger.tagged "SCHEDULER" do
+        User.schedule_tasks
+      end
+    end
+
+    # Every morning at 6:43am
+    scheduler.cron '43 6 * * *' do
+      Rails.logger.tagged "SCHEDULER" do
+        Rails.logger.info "Recalculating score values for all the tasks"
+        TaskRecord.calculate_score
+      end
+    end
   end
 end
 
-# Schedule tasks every 10 minutes
-scheduler.cron '*/10 * * * *' do
-  Rails.logger.tagged "SCHEDULER" do
-    User.schedule_tasks
-  end
-end
+SchedulerInitializer.init
 
-# Every morning at 6:43am
-scheduler.cron '43 6 * * *' do
-  Rails.logger.tagged "SCHEDULER" do
-    Rails.logger.info "Recalculating score values for all the tasks"
-    TaskRecord.calculate_score
-  end
-end
