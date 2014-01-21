1e86f78a61bf675206f07bf3661c032ce69fa151
Vikrant Shete <vikrant.shete@webstream.io>
2014-01-21 16:37:28 +0530

    Test cases added in this commit.

---
+++ spec/controllers/widgets_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe WidgetsController do  
+  
+  render_views
+
+  describe "Task Widget" do
+    before :each do
+      sign_in_admin
+    end
+
+    it "should show the tasks in descending order" do
+      tasks = FactoryGirl.create_list(:task, 10)
+      tasks.each do |task|
+        FactoryGirl.create(:work_log, :task_id => task.id)
+        if ProjectPermission.where(:project_id => task.project_id).size == 0
+          FactoryGirl.create(:project_permission, :company_id => @logged_user.company_id, :user_id => @logged_user.id, :project_id => task.project_id)
+        end
+      end
+      widget = FactoryGirl.create(:widget,
+                                  :user => @logged_user,
+                                  :company => @logged_user.company,
+                                  :configured => true,
+                                  :mine => false,
+                                  :name =>  "Recent tasks widget",
+                                  :number => 5,
+                                  :widget_type =>0,
+                                  :column => 0,
+                                  :position => 0,
+                                  :order_by => "date")
+      get :show, :id => widget.id
+      items = assigns(:items)
+      tester_array = items# []
+      tester_array = tester_array.sort_by { |t| t.created_at.to_i }
+      expect(assigns(:items)).to eq(tester_array)
+    end
+  end    
+end
