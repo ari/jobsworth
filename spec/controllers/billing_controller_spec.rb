d93cc6bbb60f514f80c91ec8fb608a44cf953067
Vikrant Shete <vikrant.shete@webstream.io>
2014-01-23 13:15:28 +0530

    Test cases have been included in this commit.

--- spec/controllers/billing_controller_spec.rb
+++ spec/controllers/billing_controller_spec.rb
@@ -7,24 +7,30 @@ describe BillingController do
     sign_in_admin
   end
   
-  it "Should pivot by date" do
-    tasks = FactoryGirl.create_list(:task_record, 20, :company_id => @logged_user.company.id)
-    tasks.each do |task|
+  it "Should generate report for pivot by date" do
+    projects = FactoryGirl.create_list( :project, 20, :company_id => @logged_user.company.id)
+    projects.each do |project|
+      task = FactoryGirl.create( :task_record,
+                                 :company_id => @logged_user.company.id,
+                                 :project_id => project.id )
+      
       FactoryGirl.create( :project_permission,
                           :user_id => @logged_user.id,
                           :company_id => @logged_user.company.id,
-                          :project_id => task.project_id )
+                          :project_id => project.id )
+      
       FactoryGirl.create( :work_log,
                           :task_id => task.id,
+                          :duration => 40,
                           :user_id => @logged_user.id,
                           :company_id => @logged_user.company.id,
-                          :project_id => task.project_id )                      
-    end
-    ROWS.each do |r|
-      get :index, { "report"=> { "type" => "1", "rows" => r, "columns" => "9", "filter_project" => "0", "filter_user" => "0", "worklog_type" => "0", "range" => "7", "start_date" => "11/11/2013", "stop_date" => "22/01/2014", "hide_approved" => "0", "hide_rejected" => "0"}}  
-      puts assigns(:column_headers).inspect
+                          :project_id => project.id )      
     end
     
-    #expect(response).to render_template("tasks/index")
+    ROWS.each do |r|
+      get :index, { "report"=> { "type" => "1", "rows" => r, "columns" => "9", "filter_project" => "0", "filter_user" => "0", "worklog_type" => "0", "range" => "7", "start_date" => "11/11/2013", "stop_date" => "24/01/2014", "hide_approved" => "0", "hide_rejected" => "0"}}  
+      assigns(:generated_report).should_not be_nil
+      response.should render_template(:layout => "basic")
+    end
   end
 end
