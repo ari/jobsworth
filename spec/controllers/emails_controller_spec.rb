1f828e349f6a364489f99f6ce4cfeffb5f8814ff
Vikrant Shete <vikrant.shete@webstream.io>
2014-01-29 20:23:32 +0530

    Modified test cases. Removed all constants used for local variable assignment.

--- spec/controllers/emails_controller_spec.rb
+++ spec/controllers/emails_controller_spec.rb
@@ -5,10 +5,11 @@ describe EmailsController do
   describe "create" do
     before :each do
       sign_in_normal_user
+      Setting.domain = "example.com" # use the same domain in squish_mail.msg
     end
   
     it "should attach a new task to a default projet" do
-      company = FactoryGirl.create( :company, :subdomain => "example" )
+      company = FactoryGirl.create( :company, :subdomain => Setting.domain.split('.')[0] )
       @logged_user.company = company
       @logged_user.save!
       project = FactoryGirl.create( :project, :company => @logged_user.company )
@@ -19,10 +20,10 @@ describe EmailsController do
                           :key => "incoming_email_project",
                           :value => project.id )
       
-      Total = TaskRecord.count                   
+      total = TaskRecord.count                   
       post :create, :secret => Setting.receiving_emails.secret, :email => File.read("squish_mail.msg")  
       response.body.should == { :success => true }.to_json
-      TaskRecord.count.should == Total + 1
+      TaskRecord.count.should == total + 1
       TaskRecord.last.customers.size.should_not == 0
       TaskRecord.last.project_id.should == project.id
     end
