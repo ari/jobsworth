74a174a4a266f1d45c6743f21e45586414292fad
Vikrant Shete <vikrant.shete@webstream.io>
2014-01-31 16:24:38 +0530

    Test cases have been added in this commit. "viks-rename-reports-on-squish-billing-page#19177" has been merged into this branch.

--- spec/controllers/billing_controller_spec.rb
+++ spec/controllers/billing_controller_spec.rb
@@ -1,37 +1,75 @@
 require 'spec_helper'
 
 describe BillingController do
-  ROWS = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 20, "property_1", "property_2", "property_3" ]
     
-  before :each do
-    sign_in_admin
-  end
-  
-  it "Should generate report for pivot by date" do
-    projects = FactoryGirl.create_list( :project, 20, :company_id => @logged_user.company.id)
-    projects.each do |project|
-      task = FactoryGirl.create( :task_record,
-                                 :company_id => @logged_user.company.id,
-                                 :project_id => project.id )
+  context "Pivot by date or days of week" do
+    
+    ROWS = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 20, "property_1", "property_2", "property_3" ]
+    
+    before :each do
+      sign_in_admin
+	  projects = FactoryGirl.create_list( :project, 20, :company_id => @logged_user.company.id)
+	  days = 30
+      projects.each do |project|
+        task = FactoryGirl.create( :task_record,
+                                   :company_id => @logged_user.company.id,
+                                   :project_id => project.id )
       
-      FactoryGirl.create( :project_permission,
-                          :user_id => @logged_user.id,
-                          :company_id => @logged_user.company.id,
-                          :project_id => project.id )
+	    FactoryGirl.create( :project_permission,
+                            :user_id => @logged_user.id,
+                            :company_id => @logged_user.company.id,
+                            :project_id => project.id )
       
-      FactoryGirl.create( :work_log,
-                          :task_id => task.id,
-                          :duration => 40,
-                          :user_id => @logged_user.id,
-                          :company_id => @logged_user.company.id,
-                          :project_id => project.id )      
+	    FactoryGirl.create( :work_log,
+							:started_at => DateTime.now - days,
+                            :task_id => task.id,
+                            :duration => 40,
+                            :user_id => @logged_user.id,
+                            :company_id => @logged_user.company.id,
+                            :project_id => project.id )
+		days -= 1
+      end
     end
-    
-    ROWS.each do |r|
-	  get :index, { "report"=> { "type" => "1", "rows" => r, "columns" => "9", "filter_project" => "0", "filter_user" => "0", "worklog_type" => "0", "range" => "7", "start_date" => "11/11/2013", "stop_date" => "24/01/2100", "hide_approved" => "0", "hide_rejected" => "0"}}  
-      assigns(:title).should_not be_nil
-      assigns(:generated_report).should_not be_nil
-      response.should render_template(:layout => "basic")
+  
+	it "Should generate report for pivot by date" do
+	  ROWS.each do |r|
+	    get :index, :report => { "type" => "1",
+							     "rows" => r,
+							     "columns" => "9",
+							     "filter_project" => "0",
+							     "filter_user" => "0",
+							     "worklog_type" => "0",
+							     "range" => "7",
+							     "start_date" => "11/11/2013",
+							     "stop_date" => "24/01/2100",
+							     "hide_approved" => "0",
+							     "hide_rejected" => "0" }  
+        assigns(:title).should_not be_nil
+        assigns(:generated_report).should_not be_nil
+        response.should render_template(:layout => "basic")
+      end
+    end
+  
+    it "should arrange column headers in ascending order" do
+	  COLUMNS = [ "7", "9" ]
+	  COLUMNS.each do |c|
+	    ROWS.each do |r|
+	      get :index, :report => { "type" => "1",
+							       "rows" => r,
+							       "columns" => c,
+							       "filter_project" => "0",
+							       "filter_user" => "0",
+							       "worklog_type" => "0",
+							       "range" => "7",
+							       "start_date" => "11/11/2013",
+							       "stop_date" => "24/01/2100",
+							       "hide_approved" => "0",
+							       "hide_rejected" => "0" }
+		  column_headers = assigns(:column_headers)
+		  column_headers.delete(:__)
+		  column_headers.keys.should == column_headers.keys.sort
+	    end
+	  end
     end
   end
 end
