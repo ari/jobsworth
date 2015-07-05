82604a6b5a2c921b29e218214b21387cb1fe52aa
Vikrant Chaudhary <vikrant@webstream.io>
2015-07-05 22:39:33 +0530

    Replace Paperclip's dependency on ImageMagick with Java.

--- config/initializers/paperclip_jruby_fix.rb
+++ config/initializers/paperclip_jruby_fix.rb
@@ -1,4 +1,17 @@
+require 'image_resizer'
+
 if RUBY_PLATFORM == 'java'
   Cocaine::CommandLine.runner = Cocaine::CommandLine::BackticksRunner.new
+
+  Paperclip::Processor.module_eval do
+    def convert_with_java arguments = "", local_options = {}
+      arguments =~ /resize "([0-9]+)x([0-9]+)"/
+      width, height = $1, $2
+      source = local_options[:source].gsub(/\[0\]$/, '')
+      dest = local_options[:dest]
+      ImageResizer.resize(source, width.to_i, height.to_i, dest)
+    end
+    alias_method_chain :convert, :java
+  end
 end
 
