b635ae6db1346018e775d4c8e9ea36bee02d957a
Vikrant Chaudhary <vikrant@webstream.io>
2015-07-07 12:09:18 +0530

    Fix an issue which caused MRI builds to fail (by requiring 'java').

--- config/initializers/paperclip_jruby_fix.rb
+++ config/initializers/paperclip_jruby_fix.rb
@@ -1,6 +1,6 @@
-require 'image_resizer'
-
 if RUBY_PLATFORM == 'java'
+  require 'image_resizer'
+
   Cocaine::CommandLine.runner = Cocaine::CommandLine::BackticksRunner.new
 
   Paperclip::Processor.module_eval do
