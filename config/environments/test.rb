5d6d77e434ff4a06bad36b4a358ce3f1123e5a90
Zaid Akram <zaidakrammughal@gmail.com>
2016-01-26 02:53:57 +0500

    Eager loaded production env. Not required in development and test environments.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,8 @@
 Jobsworth::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
+  config.eager_load = false
+
   # The test environment is used exclusively to run your application's
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
