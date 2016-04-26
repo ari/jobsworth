257f2f5944bcbe600b674e70006257ccab1d5dc8
Andrei Malyshev <andrei.malyshev@kodep.ru>
2016-04-26 17:04:33 +0300

    Fix routing error. Fixes #572

--- config/routes.rb
+++ config/routes.rb
@@ -76,6 +76,7 @@ Jobsworth::Application.routes.draw do
       get   :refresh_service_options
       get   :users_to_notify_popup
       get   :get_watcher
+      get   :get_default_customers
     end
     member do
       post :set_group
