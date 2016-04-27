6326b6cddd922c9dcae2cab2dc33748d29eff168
Andrei Malyshev <andrei.malyshev@kodep.ru>
2016-04-27 16:54:09 +0300

    Fix issue with permitted params for receiving emails.

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,13 +1,21 @@
 class EmailsController < ApplicationController
-  skip_before_filter :authenticate_user!, :only => [:create]
+  skip_before_action :verify_authenticity_token, only: :create
+  skip_before_filter :authenticate_user!, only: :create
 
   def create
-    if params[:secret]!= Setting.receiving_emails.secret
-      return render json: {success: false, message: t('error.email.secret_incorrect')}
+    if permitted_params[:secret]!= Setting.receiving_emails.secret
+      return render json: { success: false, message: t('error.email.secret_incorrect') }
     end
 
-    Mailman.receive(params[:email])
+    Mailman.receive(permitted_params[:email])
 
     render json: {success: true}
   end
+
+  private
+   
+  def permitted_params
+    params.permit!
+  end
+
 end
