d2759512cabd5b8d502768da89bc2b5bdc83db55
Andrei Malyshev <andrei.malyshev@kodep.ru>
2016-05-25 15:12:18 +0300

    Use java servlet session storage. Fixes #599

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,14 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :active_record_store,
+if defined?($servlet_context)
+  require 'action_controller/session/java_servlet_store'
+  # tell rails to use the java container's session store
+  Rails.application.config.session_store :java_servlet_store
+else
+  Rails.application.config.session_store :active_record_store,
                                        :key => '_jobsworth_session',
                                        :expire_after => 3.hours
+end
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
