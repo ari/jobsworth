c729272f9108a1aadf0f5c6ce11e3cca2ea3ba91
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2016-07-05 22:45:34 +0300

    Fix test for new time estimates format.

--- test/unit/time_parser_test.rb
+++ test/unit/time_parser_test.rb
@@ -3,32 +3,16 @@ require 'test_helper'
 class TimeParserTest < ActiveSupport::TestCase
   #   def self.format_duration(minutes)
   context 'format duration' do
-    should 'be able to format weeks' do
-      assert_equal '2w', TimeParser.format_duration(20160)
-    end
-
-    should 'be able to format days' do
-      assert_equal '2d', TimeParser.format_duration(2880)
-    end
-
     should 'be able to format hours' do
-      assert_equal '2h', TimeParser.format_duration(120)
+      assert_equal '2h00m', TimeParser.format_duration(120)
     end
 
     should 'be able to format minutes' do
-      assert_equal '4m', TimeParser.format_duration(4)
-    end
-
-    should 'be able to format weeks and days' do
-      assert_equal '2w 2d', TimeParser.format_duration(23040)
-    end
-
-    should 'be able to format days and hours' do
-      assert_equal '2d 2h', TimeParser.format_duration(3000)
+      assert_equal '0h04m', TimeParser.format_duration(4)
     end
 
     should 'be able to format hours and minutes' do
-      assert_equal '2h 10m', TimeParser.format_duration(130)
+      assert_equal '2h10m', TimeParser.format_duration(130)
     end
   end
 
