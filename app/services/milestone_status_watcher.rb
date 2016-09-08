7d47c1faa7a75447a01fae56c6264a1ba33a94b2
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2016-09-08 10:55:35 +0300

    Add log to define problem with updating milestone status.

--- app/services/milestone_status_watcher.rb
+++ app/services/milestone_status_watcher.rb
@@ -1,7 +1,12 @@
 class MilestoneStatusWatcher
 
   def self.update_status
-    Milestone.must_started_today.update_all(status: Milestone::STATUSES.index(:open))
+    Rails.logger.tagged('SCHEDULER TEST') do |logger|
+      logger.error 'Service is working.'
+      logger.error "#{Milestone.must_started_today.count} milestones are waiting update."
+      Milestone.must_started_today.update_all(status: Milestone::STATUSES.index(:open))
+      logger.error "#{Milestone.must_started_today.count} milestones are not updated."
+    end
   end
 
 end
